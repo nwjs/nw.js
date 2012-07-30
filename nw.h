ae402328b3f97b7f53a4d24da0bc8d6357a71c7d
Zhao Cheng <zcbenz@gmail.com>
2012-07-30 10:39:59 +0800

    Add new target: nw This will be our new main program, I'll move nw out of cef in future

---
+++ nw.h
@@ -0,0 +1,38 @@
+// Copyright (c) 2011 The Chromium Embedded Framework Authors. All rights
+// reserved. Use of this source code is governed by a BSD-style license that
+// can be found in the LICENSE file.
+
+#ifndef CEF_TESTS_CEFCLIENT_CEFCLIENT_H_
+#define CEF_TESTS_CEFCLIENT_CEFCLIENT_H_
+#pragma once
+
+#include <string>
+#include "include/cef_base.h"
+#include "nw/client_app.h"
+
+class CefApp;
+class CefBrowser;
+class CefCommandLine;
+
+// Returns the main browser window instance.
+CefRefPtr<CefBrowser> AppGetBrowser();
+
+// Returns the main application window handle.
+CefWindowHandle AppGetMainHwnd();
+
+// Returns the application working directory.
+std::string AppGetWorkingDirectory();
+
+// Initialize the application command line.
+void AppInitCommandLine(int argc, const char* const* argv);
+
+// Returns the application command line object.
+CefRefPtr<CefCommandLine> AppGetCommandLine();
+
+// Returns the application settings based on command line arguments.
+void AppGetSettings(CefSettings& settings, CefRefPtr<ClientApp> app);
+
+// Returns the application browser settings based on command line arguments.
+void AppGetBrowserSettings(CefBrowserSettings& settings);
+
+#endif  // CEF_TESTS_CEFCLIENT_CEFCLIENT_H_
