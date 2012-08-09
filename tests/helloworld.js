cc8fb46db94a27f14f5c3dfa9f72747c9dc44c0d
Zhao Cheng <zcbenz@gmail.com>
2012-08-09 11:18:56 +0800

    No need for old code

--- tests/helloworld.js
+++
@@ -1,5 +0,0 @@
-var nwebkit;
-nwebkit = require('../lib');
-
-nwebkit.init ({'url' : process.argv[2], 'width' : 800, 'height' : 600});
-
--- tests/runscript.js
+++
@@ -1,9 +0,0 @@
-var nwebkit;
-nwebkit = require('../lib');
-
-nwebkit.init ({'url' : "tests/testfs.html"},
-	      function() {
-		  nwebkit.context.runScript("document.body.style.background='#a3a3a3'");
-});
-
-
