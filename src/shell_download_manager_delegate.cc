b8165ece13af83a39c5fceb5406d8502ac200730
Zhao Cheng <zcbenz@gmail.com>
2012-09-04 09:54:43 +0800

    [Win] [GTK] Add support for saving download file.

--- src/shell_download_manager_delegate.cc
+++ src/shell_download_manager_delegate.cc
@@ -1,8 +1,28 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// Copyright (c) 2012 Intel Corp
+// Copyright (c) 2012 The Chromium Authors
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy 
+// of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell co
+// pies of the Software, and to permit persons to whom the Software is furnished
+//  to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in al
+// l copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IM
+// PLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNES
+// S FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+//  OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WH
+// ETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-#include "content/shell/shell_download_manager_delegate.h"
+#include "content/nw/src/shell_download_manager_delegate.h"
+
+#if defined(TOOLKIT_GTK)
+#include <gtk/gtk.h>
+#endif
 
 #if defined(OS_WIN)
 #include <windows.h>
@@ -18,12 +38,15 @@
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/download_manager.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/browser/web_contents_view.h"
 #include "net/base/net_util.h"
 
 namespace content {
 
 ShellDownloadManagerDelegate::ShellDownloadManagerDelegate()
-    : download_manager_(NULL) {
+    : download_manager_(NULL),
+      suppress_prompting_(false),
+      last_download_db_handle_(DownloadItem::kUninitializedHandle) {
   // Balanced in Shutdown();
   AddRef();
 }
@@ -44,6 +67,14 @@ void ShellDownloadManagerDelegate::Shutdown() {
 bool ShellDownloadManagerDelegate::DetermineDownloadTarget(
     DownloadItem* download,
     const DownloadTargetCallback& callback) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+  // This assignment needs to be here because even at the call to
+  // SetDownloadManager, the system is not fully initialized.
+  if (default_download_path_.empty()) {
+    default_download_path_ = download_manager_->GetBrowserContext()->GetPath().
+        Append(FILE_PATH_LITERAL("Downloads"));
+  }
+
   if (!download->GetForcedFilePath().empty()) {
     callback.Run(download->GetForcedFilePath(),
                  DownloadItem::TARGET_DISPOSITION_OVERWRITE,
@@ -65,32 +96,49 @@ bool ShellDownloadManagerDelegate::DetermineDownloadTarget(
       FROM_HERE,
       base::Bind(
           &ShellDownloadManagerDelegate::GenerateFilename,
-          this, download->GetId(), callback, generated_name));
-  return false;
+          this, download->GetId(), callback, generated_name,
+          default_download_path_));
+  return true;
 }
 
 void ShellDownloadManagerDelegate::GenerateFilename(
     int32 download_id,
     const DownloadTargetCallback& callback,
-    const FilePath& generated_name) {
-  FilePath suggested_path = download_manager_->GetBrowserContext()->GetPath().
-      Append(FILE_PATH_LITERAL("Downloads"));
-  if (!file_util::PathExists(suggested_path))
-    file_util::CreateDirectory(suggested_path);
+    const FilePath& generated_name,
+    const FilePath& suggested_directory) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::FILE));
+  if (!file_util::PathExists(suggested_directory))
+    file_util::CreateDirectory(suggested_directory);
 
-  suggested_path = suggested_path.Append(generated_name);
+  FilePath suggested_path(suggested_directory.Append(generated_name));
   BrowserThread::PostTask(
       BrowserThread::UI,
       FROM_HERE,
       base::Bind(
-          &ShellDownloadManagerDelegate::ChooseDownloadPath,
+          &ShellDownloadManagerDelegate::OnDownloadPathGenerated,
           this, download_id, callback, suggested_path));
+}
+
+void ShellDownloadManagerDelegate::OnDownloadPathGenerated(
+    int32 download_id,
+    const DownloadTargetCallback& callback,
+    const FilePath& suggested_path) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+  if (suppress_prompting_) {
+    // Testing exit.
+    callback.Run(suggested_path, DownloadItem::TARGET_DISPOSITION_OVERWRITE,
+                 DOWNLOAD_DANGER_TYPE_NOT_DANGEROUS, suggested_path);
+    return;
+  }
+
+  ChooseDownloadPath(download_id, callback, suggested_path);
 }
 
 void ShellDownloadManagerDelegate::ChooseDownloadPath(
     int32 download_id,
     const DownloadTargetCallback& callback,
     const FilePath& suggested_path) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
   DownloadItem* item =
       download_manager_->GetActiveDownloadItem(download_id);
   if (!item)
@@ -118,12 +166,47 @@ void ShellDownloadManagerDelegate::ChooseDownloadPath(
 
   if (GetSaveFileName(&save_as))
     result = FilePath(std::wstring(save_as.lpstrFile));
+#elif defined(TOOLKIT_GTK)
+  GtkWidget *dialog;
+  gfx::NativeWindow parent_window;
+  std::string base_name = FilePath(suggested_path).BaseName().value();
+
+  parent_window = item->GetWebContents()->GetView()->GetTopLevelNativeWindow();
+  dialog = gtk_file_chooser_dialog_new("Save File",
+                                       parent_window,
+                                       GTK_FILE_CHOOSER_ACTION_SAVE,
+                                       GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
+                                       GTK_STOCK_SAVE, GTK_RESPONSE_ACCEPT,
+                                       NULL);
+  gtk_file_chooser_set_do_overwrite_confirmation(GTK_FILE_CHOOSER(dialog),
+                                                 TRUE);
+  gtk_file_chooser_set_current_name(GTK_FILE_CHOOSER(dialog),
+                                    base_name.c_str());
+
+  if (gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_ACCEPT) {
+    char *filename;
+    filename = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
+    result = FilePath(filename);
+  }
+  gtk_widget_destroy(dialog);
 #else
   NOTIMPLEMENTED();
 #endif
 
   callback.Run(result, DownloadItem::TARGET_DISPOSITION_PROMPT,
                DOWNLOAD_DANGER_TYPE_NOT_DANGEROUS, result);
+}
+
+void ShellDownloadManagerDelegate::AddItemToPersistentStore(
+    DownloadItem* item) {
+  download_manager_->OnItemAddedToPersistentStore(
+      item->GetId(), --last_download_db_handle_);
+}
+
+void ShellDownloadManagerDelegate::SetDownloadBehaviorForTesting(
+    const FilePath& default_download_path) {
+  default_download_path_ = default_download_path;
+  suppress_prompting_ = true;
 }
 
 }  // namespace content
--- src/shell_download_manager_delegate.h
+++ src/shell_download_manager_delegate.h
@@ -1,9 +1,25 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// Copyright (c) 2012 Intel Corp
+// Copyright (c) 2012 The Chromium Authors
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy 
+// of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell co
+// pies of the Software, and to permit persons to whom the Software is furnished
+//  to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in al
+// l copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IM
+// PLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNES
+// S FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+//  OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WH
+// ETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+//  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-#ifndef CONTENT_SHELL_SHELL_DOWNLOAD_MANAGER_DELEGATE_H_
-#define CONTENT_SHELL_SHELL_DOWNLOAD_MANAGER_DELEGATE_H_
+#ifndef CONTENT_NW_SRC_SHELL_DOWNLOAD_MANAGER_DELEGATE_H_
+#define CONTENT_NW_SRC_SHELL_DOWNLOAD_MANAGER_DELEGATE_H_
 
 #include "base/compiler_specific.h"
 #include "base/memory/ref_counted.h"
@@ -25,6 +41,11 @@ class ShellDownloadManagerDelegate
   virtual bool DetermineDownloadTarget(
       DownloadItem* download,
       const DownloadTargetCallback& callback) OVERRIDE;
+  virtual void AddItemToPersistentStore(DownloadItem* item) OVERRIDE;
+
+  // Inhibits prompting and sets the default download path.
+  void SetDownloadBehaviorForTesting(
+      const FilePath& default_download_path);
 
  private:
   friend class base::RefCountedThreadSafe<ShellDownloadManagerDelegate>;
@@ -33,16 +54,23 @@ class ShellDownloadManagerDelegate
 
   void GenerateFilename(int32 download_id,
                         const DownloadTargetCallback& callback,
-                        const FilePath& generated_name);
+                        const FilePath& generated_name,
+                        const FilePath& suggested_directory);
+  void OnDownloadPathGenerated(int32 download_id,
+                               const DownloadTargetCallback& callback,
+                               const FilePath& suggested_path);
   void ChooseDownloadPath(int32 download_id,
                           const DownloadTargetCallback& callback,
                           const FilePath& suggested_path);
 
   DownloadManager* download_manager_;
+  FilePath default_download_path_;
+  bool suppress_prompting_;
+  int64 last_download_db_handle_;
 
   DISALLOW_COPY_AND_ASSIGN(ShellDownloadManagerDelegate);
 };
 
 }  // namespace content
 
-#endif  // CONTENT_SHELL_SHELL_DOWNLOAD_MANAGER_DELEGATE_H_
+#endif  // CONTENT_NW_SRC_SHELL_DOWNLOAD_MANAGER_DELEGATE_H_
