505c504e9c6ab36ee38aaa7c4cc612259c5ff70b
Roger WANG <roger.wang@intel.com>
2013-02-22 17:09:05 +0800

    adding logging for our IPC messages

--- src/shell_main_delegate.cc
+++ src/shell_main_delegate.cc
@@ -52,6 +52,16 @@
 #include <initguid.h>
 #endif
 
+#include "ipc/ipc_message.h"  // For IPC_MESSAGE_LOG_ENABLED.
+
+#if defined(IPC_MESSAGE_LOG_ENABLED)
+#define IPC_MESSAGE_MACROS_LOG_ENABLED
+#include "content/public/common/content_ipc_logging.h"
+#define IPC_LOG_TABLE_ADD_ENTRY(msg_id, logger) \
+    content::RegisterIPCLogger(msg_id, logger)
+#include "content/nw/src/api/api_messages.h"
+#endif
+
 namespace {
 
 #if defined(OS_WIN)
