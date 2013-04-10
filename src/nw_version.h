618e4a33ac192cdc15b6e827310d7536723158af
Roger WANG <roger.wang@intel.com>
2013-04-10 13:35:54 +0800

    bump version to 0.5.0-rc

--- src/nw_version.h
+++ src/nw_version.h
@@ -22,8 +22,8 @@
 #define NW_VERSION_H
 
 #define NW_MAJOR_VERSION 0
-#define NW_MINOR_VERSION 4
-#define NW_PATCH_VERSION 3
+#define NW_MINOR_VERSION 5
+#define NW_PATCH_VERSION 0
 #define NW_VERSION_IS_RELEASE 0
 
 #ifndef NW_STRINGIFY
@@ -38,7 +38,7 @@
 #else
 # define NW_VERSION_STRING  NW_STRINGIFY(NW_MAJOR_VERSION) "." \
                               NW_STRINGIFY(NW_MINOR_VERSION) "." \
-                              NW_STRINGIFY(NW_PATCH_VERSION) "-pre"
+                              NW_STRINGIFY(NW_PATCH_VERSION) "-rc"
 #endif
 
 #define NW_VERSION "v" NW_VERSION_STRING
