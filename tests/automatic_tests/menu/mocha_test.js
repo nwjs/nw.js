2675fa5a2aa617906ab686619534c6df1fef3479
Kaiyang Lv(Kevin Lui) <kevinlui598@gmail.com>
2013-07-11 12:49:34 +0800

    [test] merge manual test case menu to automatic

--- tests/automatic_tests/menu/mocha_test.js
+++ tests/automatic_tests/menu/mocha_test.js
@@ -78,6 +78,93 @@ describe('Menu', function(){
   describe('#popup(), not implentment',function(){
   })
 
+    describe('#.items', function() {
+	var menu = new gui.Menu();
+
+	it('should be emtpy', function() {
+	    assert.equal(menu.items.length, 0);
+	    assert.equal(typeof menu.click, 'undefined');
+	});
+
+	
+	it('should be one', function() {
+	    menu.append(new gui.MenuItem({
+		label: 'Item 1',
+		click: function() {
+		    return 'Click on Item 1';
+		}
+	    }));
+
+	    assert.equal(menu.items.length, 1);
+	    assert.equal(menu.items[0].label, 'Item 1');
+	    assert.equal(menu.items[0].icon, '');
+	    assert.equal(menu.items[0].tooltip, '');
+	    assert.equal(typeof menu.items[0].checked, 'undefined');
+	    assert.equal(menu.items[0].enabled, true);
+	});
+	
+	
+	it('should be more', function() {
+
+	    menu.append(new gui.MenuItem({ type: 'separator' }));
+	    menu.append(new gui.MenuItem({
+		label: 'Item 2',
+		click: function() {
+		    return 'Click on Item 2';
+		}
+	    }));
+	    menu.append(new gui.MenuItem({ label: 'Item 3' }));
+	    menu.append(new gui.MenuItem({ label: 'Item 4', tooltip: 'tooltip' }));
+	    menu.append(new gui.MenuItem({ label: 'Item 5' }));
+	    menu.insert(new gui.MenuItem({ label: 'Item Papa' }), 1);
+	    
+	    assert.equal(menu.items.length, 7);
+	    assert.equal(menu.items[0].label, 'Item 1');
+	    assert.equal(menu.items[1].label, 'Item Papa');
+	    assert.equal(menu.items[2].type, 'separator');
+	    assert.equal(menu.items[3].label, 'Item 2');
+	    assert.equal(menu.items[4].label, 'Item 3');
+	    assert.equal(menu.items[4].tooltip, '');
+	    assert.equal(menu.items[5].label, 'Item 4');
+	    assert.equal(menu.items[5].tooltip, 'tooltip');
+	    assert.equal(menu.items[6].label, 'Item 5');
+	});
+
+    });
+    
+    describe('menubar', function() {
+	var win = gui.Window.get();
+	var menubar = new gui.Menu({ type: 'menubar' });
+	var sub1 = new gui.Menu();
+	sub1.append(new gui.MenuItem({
+	    label: 'Test1',
+	    click: function() {
+		return 'menubar sub1 Test1';
+	    }
+	}));
+	
+	sub1.append(new gui.MenuItem({
+	    type: 'checkbox',
+	    checked: true, //<------------------------------------------------!!!!!!
+	    label: 'Test CheckBox'
+	}));
+	
+	var sub2 = new gui.Menu();
+	sub2.append(new gui.MenuItem({
+	    label: 'Test2',
+	    click: function() {
+		return 'menubar sub2 Test2';
+	    }
+	}));
+	
+	menubar.append(new gui.MenuItem({ label: 'Sub1', submenu: sub1}));
+	menubar.append(new gui.MenuItem({ label: 'Sub2', submenu: sub2}));
+	it('should set menubar', function() {
+	    assert.equal(typeof win.menu, 'undefined')
+	    win.menu = menubar;
+	    assert.equal(win.menu, menubar);
+	});
+    })
 })
 
 
