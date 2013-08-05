123b6c4898d79ddcfc3ecd016b5904aa6e32d8a0
Kaiyang Lv(Kevin Lui) <kevinlui598@gmail.com>
2013-08-05 11:27:33 +0800

    Add cache support for local server

--- tests/server/server.js
+++ tests/server/server.js
@@ -2,8 +2,11 @@ var http = require("http"),
 url = require("url"),
 path = require("path"),
 fs = require("fs"), 
+config = require('./config');
 ports = new Array(8123,8124),
 servers = new Array(ports.length);
+
+var res_save = new Array();
 
 types = {
     ".css": "text/css",
@@ -39,11 +42,25 @@ function request_listener(req, res) {
 
     path.exists(pathname,function(exists){
 	if(exists){
-	    var content_type = types[path.extname(pathname)] || "text/plain";
-	    res.writeHead(200, {"Content-Type": content_type});
+	    var ext = path.extname(pathname);
+	    var content_type = types[ext] || "text/plain";
 
-	    fs.readFile(pathname,function (err,data){
-		res.end(data);
+	    res.setHeader("Content-Type", content_type);
+
+	    fs.stat(pathname, function (err, stat) {
+		var last_modified = stat.mtime.toUTCString();
+		var if_modified_since = "If-Modified-Since".toLowerCase();
+		res.setHeader("Last-Modified", last_modified);
+		
+		if (req.headers[if_modified_since] && last_modified == req.headers[if_modified_since]) {
+		    res_save.push({"status": 304, 'pathname':req.url.slice(1)});
+		    res.writeHead(304, "Not Modified");
+		    res.end();
+		} else {
+		    fs.readFile(pathname,function (err,data){
+			res.end(data);
+		    });
+		}
 	    });
 	} else {
 	    res.writeHead(404, {"Content-Type": "text/html"});
@@ -65,3 +82,5 @@ for (var i = 0; i < ports.length; i++) {
     servers[i].on('error', error_handler);
     console.log("Server running at http://127.0.0.1:" + ports[i]);
 }
+
+exports.res_save = res_save;
