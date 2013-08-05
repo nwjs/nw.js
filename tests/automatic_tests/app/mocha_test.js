c0a151f00bc8d7a2a4d2a97d44218bae30ceb14d
Kaiyang Lv(Kevin Lui) <kevinlui598@gmail.com>
2013-08-05 12:21:37 +0800

    Merge test case for App.manifest

---
+++ tests/automatic_tests/app/mocha_test.js
@@ -0,0 +1,24 @@
+var gui = require('nw.gui');
+var assert = require('assert');
+
+describe('gui.App', function() {
+    describe('manifest', function() {
+	it('`gui.App.manifest` should equle to value of package.json', function() {
+	    assert.equal(gui.App.manifest.name, 'nw-tests');       
+	});
+	
+	it('have window', function() {
+	    assert.equal(typeof gui.App.manifest.window, 'object');       
+	});
+	
+	it('have main', function() {
+	    assert.equal(typeof gui.App.manifest.main, 'string');
+	    assert.equal(gui.App.manifest.main, 'index.html');
+	});
+	
+	it('have dependencies', function() {
+	    assert.equal(typeof gui.App.manifest.dependencies, 'object');       
+	});
+    });
+    
+})
