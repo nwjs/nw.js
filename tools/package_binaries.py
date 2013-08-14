43b6218f2fa1a0588c4729d37458f0ac6999ae55
Kaiyang Lv(Kevin Lui) <kevinlui598@gmail.com>
2013-08-14 12:50:51 +0800

    Make the 'dist' build target depends on 'chromedriver2_server' target. And  make an additional tarball/zip to distribute the chromedriver2_server binary.

--- tools/package_binaries.py
+++ tools/package_binaries.py
@@ -10,7 +10,13 @@ nw_root  = os.path.normpath(os.path.join(script_dir, os.pardir))
 project_root = os.path.normpath(os.path.join(nw_root, os.pardir, os.pardir)); 
 #default project path
 project_root = os.path.join(project_root, 'out', 'Release')
-
+required_file = []
+tarname = []
+binary_name = []
+binary_tar = []
+binary_store_path = []
+binary_full_path = []
+binary_tar_full_path = []
 
 #parse command line arguments
 """
@@ -56,6 +62,11 @@ if platform_name == 'osx' and not os.path.exists(
   print 'nw file does not exist.\n'
   exit()
 
+if not os.path.exists(
+  os.path.join(project_root, 'chromedriver2_server')):
+  print 'chromedriver2_server file does not exist.\n'
+  exit()
+
 required_file_linux = (
   'nw',
   'nw.pak',
@@ -78,15 +89,20 @@ required_file_mac = (
   'nwsnapshot',
 )
 
+required_chromedriver2_file = (
+  'chromedriver2_server',
+)
 
 if (platform_name == 'linux'):
-  required_file = required_file_linux
+  required_file.append(required_file_linux)
 
 if (platform_name == 'win'):
-  required_file = required_file_win
+  required_file.append(required_file_win)
 
 if (platform_name == 'osx'):
-  required_file = required_file_mac
+  required_file.append(required_file_mac)
+
+required_file.append(required_chromedriver2_file)
 
 #generate binary tar name
 import getnwisrelease
@@ -108,82 +124,87 @@ else:
 if (platform_name == 'win' or platform_name == 'osx'):
   arch = 'ia32'
 
-tarname = 'node-webkit-' + nw_version
-
-binary_name = tarname + '-' + platform_name + '-' + arch
-binary_tar = binary_name + '.tar.gz'
+tarname.append('node-webkit-' + nw_version)
+tarname.append('chromedriver2-nw-' + nw_version)
+for index in range(len(tarname)):
+  binary_name.append(tarname[index] + '-' + platform_name + '-' + arch);
+  binary_tar.append(binary_name[index] + '.tar.gz')
 
 #use zip in mac and windows
 if platform_name in ('win', 'osx'):
-  binary_tar = binary_name + '.zip'
+  for index in range(len(binary_name)):
+    binary_tar[index] = binary_name[index] + '.zip'
 
 
 #make directory for binary_tar
-binary_store_path = os.path.join(project_root,                                 
-                                 'node-webkit-binaries')
+binary_store_path.append(os.path.join(project_root,                                 
+                                 'node-webkit-binaries'))
+binary_store_path.append(os.path.join(project_root,                                 
+                                 'chromedriver2-binaries'))
 
+for index in range(len(binary_store_path)):
+  if not os.path.exists(binary_store_path[index]):
+    os.mkdir(binary_store_path[index])
 
-if not os.path.exists(binary_store_path):
-  os.mkdir(binary_store_path)
+for index in range(len(binary_store_path)):
+  binary_full_path.append(os.path.join(binary_store_path[index], binary_name[index]))
+  binary_tar_full_path.append(os.path.join(binary_store_path[index], binary_tar[index]))
 
+for index in range(len(binary_full_path)):
+  if os.path.exists(binary_full_path[index]):
+    shutil.rmtree(binary_full_path[index])
 
-binary_full_path = os.path.join(binary_store_path, binary_name)
-binary_tar_full_path = os.path.join(binary_store_path, binary_tar)
-
-
-if os.path.exists(binary_full_path):
-  shutil.rmtree(binary_full_path)
-
-os.mkdir(binary_full_path)
+for index in range(len(binary_full_path)):
+  os.mkdir(binary_full_path[index])
 
 
 #copy file to binary
 print 'Begin copy file.'
-for file in required_file:
-  try:
-    shutil.copy(os.path.join(project_root, file),
-                os.path.join(binary_full_path, file))
-  except:
-    shutil.copytree(os.path.join(project_root, file),
-                    os.path.join(binary_full_path, file))
+for index in range(len(required_file)):
+  for file in required_file[index]:
+    try:
+      shutil.copy(os.path.join(project_root, file),
+                  os.path.join(binary_full_path[index], file))
+    except:
+      shutil.copytree(os.path.join(project_root, file),
+                      os.path.join(binary_full_path[index], file))
     
 print 'copy file end.\n'
 
-if (os.path.isfile(binary_tar_full_path)):
-  os.remove(binary_tar_full_path)
-  
+for index in range(len(binary_tar_full_path)):
+  if (os.path.isfile(binary_tar_full_path[index])):
+    os.remove(binary_tar_full_path[index])
   
 print 'Begin compress file'
-
 
 if platform_name in ('win', 'osx'):
   """
   If there are sub directors, this should be modified  
   """
   import zipfile
-  zip = zipfile.ZipFile(binary_tar_full_path, 'w',
-                      compression=zipfile.ZIP_DEFLATED)
+  for index in range(len(binary_tar_full_path)):                     
+    zip = zipfile.ZipFile(binary_tar_full_path[index], 'w',
+                        compression=zipfile.ZIP_DEFLATED)
 
-  for dirpath, dirnames, filenames in os.walk(binary_full_path):
-    for name in filenames:
-      path = os.path.normpath(os.path.join(dirpath, name))
+    for dirpath, dirnames, filenames in os.walk(binary_full_path[index]):
+      for name in filenames:
+        path = os.path.normpath(os.path.join(dirpath, name))
       
-      if os.path.isfile(path):
-        zip.write(path, os.path.join(os.path.basename(binary_full_path), name))
-    
+        zip.write(path, path.replace(binary_full_path[index] + os.sep, ''))
 
-  zip.close()
+    zip.close()
 
 else:
-  tar = tarfile.open(binary_tar_full_path, 'w:gz')
-  tar.add(binary_full_path, os.path.basename(binary_full_path))
-  tar.close()
+  for index in range(len(binary_tar_full_path)):                       
+    tar = tarfile.open(binary_tar_full_path[index], 'w:gz')
+    tar.add(binary_full_path[index], os.path.basename(binary_full_path[index]))
+    tar.close()
 
   
 print 'compress file end.\n'
 
-
-print 'the binaries files store in path:', os.path.normpath(
-    os.path.join(os.getcwd(), binary_store_path))
+for index in range(len(binary_store_path)):
+  print 'the binaries files store in path:', os.path.normpath(
+      os.path.join(os.getcwd(), binary_store_path[index]))
 
 
