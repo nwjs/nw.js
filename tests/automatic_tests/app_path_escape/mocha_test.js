81db6de519b4df6e0553bbced6416ce4327d61b5
richardcypher <sjturichardcypher@gmail.com>
2013-08-19 15:35:49 +0800

    fix bugs of useragentapp and pathescape test cases

--- tests/automatic_tests/app_path_escape/mocha_test.js
+++ tests/automatic_tests/app_path_escape/mocha_test.js
@@ -3,11 +3,18 @@ var os = require('os');
 describe('app_path_escaping',function(){
   it('app should start normally with directory named xdk',function(done){
     this.timeout(0);
+    var opened = false;
     if(os.platform() == "win32"){
       var app = spawn(process.execPath, [path.join(global.tests_dir, 'app_path_escape/xdk')]);
+      app.on('close',function(){
+        opened = true;
+        done();
+      })
       setTimeout(function() {
-        app.kill();
-        done('the app is not executed');
+        if(!opened){
+          app.kill();
+          done('the app is not executed');
+        }
       }, 5000);
     }
     else
