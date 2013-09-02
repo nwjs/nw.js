4953a6131674fbcac02c1ef2287a9d8f07778466
Roger <wenrui@gmail.com>
2013-09-02 13:47:56 +0800

    set transport_security_state for HTTPS connections
    So we can launch https sites in debug mode

--- src/net/shell_url_request_context_getter.cc
+++ src/net/shell_url_request_context_getter.cc
@@ -134,6 +134,7 @@ net::URLRequestContext* ShellURLRequestContextGetter::GetURLRequestContext() {
         net::HostResolver::CreateDefaultResolver(NULL));
 
     storage_->set_cert_verifier(net::CertVerifier::CreateDefault());
+    storage_->set_transport_security_state(new net::TransportSecurityState);
 
     net::ProxyService* proxy_service;
     net::DhcpProxyScriptFetcherFactory dhcp_factory;
@@ -165,6 +166,8 @@ net::URLRequestContext* ShellURLRequestContextGetter::GetURLRequestContext() {
     net::HttpNetworkSession::Params network_session_params;
     network_session_params.cert_verifier =
         url_request_context_->cert_verifier();
+    network_session_params.transport_security_state =
+        url_request_context_->transport_security_state();
     network_session_params.server_bound_cert_service =
         url_request_context_->server_bound_cert_service();
     network_session_params.proxy_service =
