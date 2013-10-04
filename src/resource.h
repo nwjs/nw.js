985464e4d39b70dc643bbf064d27b90968af18e9
trevorlinton <trevor.linton@gmail.com>
2013-10-03 21:53:12 -0600

    added back resource.h

---
+++ src/resource.h
@@ -0,0 +1,37 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by test_shell.rc
+//
+
+
+#define IDD_ALERT                 130
+#define IDD_CONFIRM               131
+#define IDD_PROMPT                132
+#define IDD_LOGIN                 133
+#define IDC_PROMPTEDIT            1005
+#define IDC_DIALOGTEXT            1006
+#define IDC_DIALOG_USERNAMETEXT   1007
+#define IDC_DIALOG_PASSWORDTEXT   1008
+#define IDC_DIALOG_MESSAGETEXT    1009
+#define IDC_USERNAMEEDIT          1010
+#define IDC_PASSWORDEDIT          1011
+
+#ifndef IDC_STATIC
+#define IDC_STATIC                -1
+#endif
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC               130
+#define _APS_NEXT_RESOURCE_VALUE  129
+#define _APS_NEXT_COMMAND_VALUE   32771
+#define _APS_NEXT_CONTROL_VALUE   1000
+#define _APS_NEXT_SYMED_VALUE     117
+#endif
+#endif
