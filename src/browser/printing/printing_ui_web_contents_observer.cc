a8189d926ea086d2e670dc839720d3ef37e93afa
Roger <wenrui@gmail.com>
2013-12-25 20:07:17 +0800

    add missing printing_ui_web_contents_observer files

---
+++ src/browser/printing/printing_ui_web_contents_observer.cc
@@ -0,0 +1,20 @@
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/nw/src/browser/printing/printing_ui_web_contents_observer.h"
+
+#include "content/public/browser/browser_thread.h"
+#include "content/public/browser/web_contents.h"
+#include "content/public/browser/web_contents_view.h"
+
+PrintingUIWebContentsObserver::PrintingUIWebContentsObserver(
+    content::WebContents* web_contents)
+    : content::WebContentsObserver(web_contents) {
+  DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
+}
+
+gfx::NativeView PrintingUIWebContentsObserver::GetParentView() {
+  DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
+  return web_contents() ? web_contents()->GetView()->GetNativeView() : NULL;
+}
