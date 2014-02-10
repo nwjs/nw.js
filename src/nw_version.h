dff6e53a9e1e8386ca715a98aaa6e4ff8d9efb4c
Roger <wenrui@gmail.com>
2014-02-10 13:40:19 +0800

    update chromium to 32.0.1700.107

--- src/nw_version.h
+++ src/nw_version.h
@@ -43,7 +43,7 @@
 
 #define NW_VERSION "v" NW_VERSION_STRING
 
-#define CHROME_VERSION "32.0.1700.77"
+#define CHROME_VERSION "32.0.1700.107"
 
 #define NW_VERSION_AT_LEAST(major, minor, patch) \
   (( (major) < NW_MAJOR_VERSION) \
