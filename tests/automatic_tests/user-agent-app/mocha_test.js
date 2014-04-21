067c1529e0d54e6c7bbb52be6d0ee84c0395f132
tinyproxy <tinyproxy@gmail.com>
2014-04-21 13:10:05 +0800

    Clear crontab

--- tests/automatic_tests/user-agent-app/mocha_test.js
+++ tests/automatic_tests/user-agent-app/mocha_test.js
@@ -7,27 +7,30 @@ describe('user-agent-app', function() {
     function(done) {
       this.timeout(0);
       var result = false;
+      var crontab = undefined;
 
       var child = app_test.createChildProcess({
         execPath: process.execPath,
         appPath: path.join(global.tests_dir, 'user-agent-app'),
         end: function(data, app) {
-			result = true;
-			var package_info = JSON.parse(fs.readFileSync('automatic_tests/user-agent-app/package.json', 'utf8'));
-			app.kill();
-      if (data == package_info.name) {
-				done();
-			}
-			else{
-				done('the user agent has changed');
-			}
+          if (crontab != undefined){
+            clearTimeout(crontab);
+          }
+          result = true;
+          var package_info = JSON.parse(fs.readFileSync('automatic_tests/user-agent-app/package.json', 'utf8'));
+          app.kill();
+          if (data == package_info.name) {
+            done();
+          } else {
+            done('the user agent has changed');
+          }
         }
       });
-      setTimeout(function(){
-          if (!result) {
-            child.close();
-            done('loaded event does not been fired');
-          }
-        }, 10000);
-  })
+      crontab = setTimeout(function() {
+        if (!result) {
+          child.close();
+          done('loaded event does not been fired');
+        }
+      }, 10000);
+    })
 })
