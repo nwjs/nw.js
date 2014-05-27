9a99dbcd873543b0da762bad7e0d8a9f32c3a220
Roger Wang <roger.wang@intel.com>
2014-05-27 09:09:54 +0800

    [OSX] Skip key equivlant handling for menus
    This fixes the latency of key strokes. Cocoa will try to populate
    the menu (and then we send the 'show' event) on keypress of key
    in shortcuts.

--- src/api/menu/menu_delegate_mac.mm
+++ src/api/menu/menu_delegate_mac.mm
@@ -34,15 +34,21 @@
   return self;
 }
 
+- (BOOL)menuHasKeyEquivalent:(NSMenu *)menu forEvent:(NSEvent *)event target:(id *)target action:(SEL *)action {
+  return NO;
+}
+
 - (void)menuNeedsUpdate:(NSMenu*)menu {
   if (!nwmenu_->enable_show_event() || nwmenu_->dispatcher_host()->run_loop())
     return;
-  NSEvent* event = [NSApp currentEvent];
+
+  // NSEvent* event = [NSApp currentEvent];
   // NSLog (@"%@\n", event);
   // Cocoa will try to populate menu on every keystoke of the key equivlants,
   // which is slow. The following bypassed it
-  if ([event type] != NSSystemDefined || [event subtype] == 8)
-    return;
+  // if ([event type] != NSSystemDefined || [event subtype] == 8)
+  //  return;
+
   base::ListValue args;
   base::RunLoop run_loop;
   nwmenu_->dispatcher_host()->set_run_loop(&run_loop);
