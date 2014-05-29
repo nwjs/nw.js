a4f7660a61859ce734ac6b1481985c8f4600098a
libm <li@b.m>
2014-05-29 13:37:46 +0800

    Move aws key id settings into boto config file.
    Goto code.google.com/p/boto/wiki/BotoConfig for more information

--- tools/aws_uploader.py
+++ tools/aws_uploader.py
@@ -5,16 +5,20 @@ import datetime
 import json
 import os
 
+
+# Set timeout, for retry
+#if not boto.config.has_section('Boto'):
+#    boto.config.add_section('Boto')
+#boto.config.set('Boto','http_socket_timeout','30')
+
 ################################
 # Parse command line args
-parser = argparse.ArgumentParser(description='AWS uploader.')
+parser = argparse.ArgumentParser(description='AWS uploader, please fill in your aws key and id in Boto config (~/.boto)')
 parser.add_argument('-p','--path', help='Optional. Where to find the binaries, normally out/Release/dist', required=False)
 parser.add_argument('-b','--buildername', help='Builder name, e.g. linux_32bit', required=True)
 parser.add_argument('-r','--revision', help='Commit revision',required=True)
 parser.add_argument('-n','--number', help='Build number', required=True)
 parser.add_argument('-t','--bucket', help='AWS bucket name', required=True)
-parser.add_argument('-k','--keyfile', help='Optional. JSON file containing AWS access id and key, default: ~/.awskey; ' +\
-        'The content of the key file is like {"awsid":"ABCDEF","awskey":"123456"}', required=False)
 
 args = parser.parse_args()
 
@@ -25,7 +29,6 @@ builder_name = args.buildername
 got_revision = args.revision
 build_number = args.number
 bucket_name  = args.bucket
-keyfile      = args.keyfile
 date         = datetime.date.today().strftime('%m-%d-%Y')
 
 # If the binaries location is not given, calculate it from script related dir.
@@ -40,22 +43,6 @@ if not os.path.isdir(dist_dir):
     print 'Invalid path: ' + dist_dir
     exit(-1)
 dist_dir = os.path.normpath(dist_dir)
-
-# Check aws keyfile
-if keyfile == None:
-    keyfile = os.path.join(os.path.expanduser('~'),'.awskey')
-    print keyfile
-if not os.path.exists(keyfile):
-    print "Cannot find aws key file"
-    exit(-1)
-
-json_data = open(keyfile)
-data = json.load(json_data)
-if not (data.has_key('awsid') and data.has_key('awskey')):
-    print "Invalid key file format."
-    exit(-1)
-awsid = data['awsid']
-awskey = data['awskey']
 
 # it's for S3, so always use '/' here
 upload_path = ''.join(['/' + date,
@@ -72,7 +59,7 @@ def print_progress(transmitted, total):
 
 
 def aws_upload(upload_path, file_list):
-    conn = boto.connect_s3(awsid, awskey)
+    conn = boto.connect_s3()
     print 'Connecting to S3 ...'
     bucket = conn.get_bucket(bucket_name)
     print 'Uploading to: ' + upload_path
