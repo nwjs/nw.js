193e8e670d974a394c5ac51c22f6387f69ca32da
Roger <wenrui@gmail.com>
2014-11-06 11:09:16 +0800

    [WIN] enable high DPI support
    Fix #2524

--- src/shell_main.cc
+++ src/shell_main.cc
@@ -20,12 +20,15 @@
 
 #include "content/public/app/content_main.h"
 
+#include "base/command_line.h"
 #include "content/nw/src/shell_main_delegate.h"
 #include "sandbox/win/src/sandbox_types.h"
 
 #if defined(OS_WIN)
 #include "base/win/win_util.h"
+#include "base/win/windows_version.h"
 #include "content/public/app/startup_helper_win.h"
+#include "ui/gfx/win/dpi.h"
 #endif
 
 #if defined(OS_MACOSX)
@@ -35,6 +38,11 @@
 #if defined(OS_WIN)
 
 int APIENTRY wWinMain(HINSTANCE instance, HINSTANCE, wchar_t*, int) {
+  CommandLine::Init(0, NULL);
+  if (base::win::GetVersion() > base::win::VERSION_VISTA)
+    gfx::EnableHighDPISupport();
+
+
   sandbox::SandboxInterfaceInfo sandbox_info = {0};
   content::InitializeSandboxInfo(&sandbox_info);
   content::ShellMainDelegate delegate;
