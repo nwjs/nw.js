7a4ca7ed3a59ccfbdd189a622f39f82d17bd9821
Roger <wenrui@gmail.com>
2015-02-02 10:40:33 +0800

    update chrome version string to 41.0.2272.32

--- src/nw_version.h
+++ src/nw_version.h
@@ -43,7 +43,7 @@
 
 #define NW_VERSION "v" NW_VERSION_STRING
 
-#define CHROME_VERSION "41.0.2236.2"
+#define CHROME_VERSION "41.0.2272.32"
 
 #define NW_VERSION_AT_LEAST(major, minor, patch) \
   (( (major) < NW_MAJOR_VERSION) \
