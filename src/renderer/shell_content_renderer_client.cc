efdc156fe42e42e8efbd5bea6f6276efd1f549f1
Roger <wenrui@gmail.com>
2015-02-20 23:39:33 +0800

    move setting embedder data of Node from blink
    together with be948af in Blink, Fix #2996
    
    Conflicts:
    	src/renderer/shell_content_renderer_client.cc

--- src/renderer/shell_content_renderer_client.cc
+++ src/renderer/shell_content_renderer_client.cc
@@ -62,6 +62,9 @@
 #include "third_party/WebKit/public/web/WebSecurityOrigin.h"
 #include "third_party/WebKit/public/web/WebSecurityPolicy.h"
 #include "third_party/WebKit/public/web/WebView.h"
+#include "gin/public/context_holder.h"
+#include "gin/public/gin_embedders.h"
+
 //#include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebString.h"
 #include "content/common/dom_storage/dom_storage_map.h"
 
@@ -233,6 +236,15 @@ void ShellContentRendererClient::DidCreateScriptContext(
   VLOG(1) << "DidCreateScriptContext: " << url;
   InstallNodeSymbols(frame, context, url);
   creating_first_context_ = false;
+
+#if 1 //defined(NW_IMPLEMENTATION)
+    v8::Local<v8::Context> node_context =
+        v8::Local<v8::Context>::New(context->GetIsolate(), node::g_context);
+    //need to stay sync with V8PerContextData.h
+    int v8ContextPerContextDataIndex = static_cast<int>(gin::kPerContextDataStartIndex + gin::kEmbedderBlink);
+    node_context->SetAlignedPointerInEmbedderData(v8ContextPerContextDataIndex,
+                                                  context->GetAlignedPointerFromEmbedderData(v8ContextPerContextDataIndex));
+#endif
 }
 
 bool ShellContentRendererClient::goodForNode(blink::WebFrame* frame)
