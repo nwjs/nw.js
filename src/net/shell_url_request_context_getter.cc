899d022b804310d2c6133081731bbf09fe7e9183
Roger <wenrui@gmail.com>
2015-04-13 20:19:37 +0800

    Fix #2819: enable cookie support for web sockets

--- src/net/shell_url_request_context_getter.cc
+++ src/net/shell_url_request_context_getter.cc
@@ -187,8 +187,8 @@ net::URLRequestContext* ShellURLRequestContextGetter::GetURLRequestContext() {
     cookie_store->GetCookieMonster()->SetPersistSessionCookies(true);
     storage_->set_cookie_store(cookie_store);
 
-    const char* schemes[] = {"http", "https", "file", "app"};
-    cookie_store->GetCookieMonster()->SetCookieableSchemes(schemes, 4);
+    const char* schemes[] = {"http", "https", "ws", "wss", "app", "file"};
+    cookie_store->GetCookieMonster()->SetCookieableSchemes(schemes, 6);
 
     storage_->set_channel_id_service(new net::ChannelIDService(
         new net::DefaultChannelIDStore(NULL),
