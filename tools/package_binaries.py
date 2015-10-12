51d5700963a99cbff59db80cd405cbd3f4db6567
Roger Wang <roger.wang@intel.com>
2015-10-12 21:36:13 +0800

    make lib in linux dist folder

--- tools/package_binaries.py
+++ tools/package_binaries.py
@@ -383,6 +383,8 @@ def make_packages(targets):
             # copy files into a folder then pack
             folder = os.path.join(dist_dir, t['output'])
             os.mkdir(folder)
+            if platform_name == 'linux':
+                os.mkdir(os.path.join(folder, 'lib'))
             for f in t['input']:
                 src = os.path.join(binaries_location, f)
                 dest = os.path.join(folder, f)
