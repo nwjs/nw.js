5ef29d52a61621d64aa3667da9f9663da5408895
Roger Wang <roger.wang@intel.com>
2015-10-22 13:12:46 +0800

    [test] remote/tar

---
+++ test/remoting/tar/test.py
@@ -0,0 +1,23 @@
+import time
+import os
+import shutil
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+testdir = os.path.dirname(os.path.abspath(__file__))
+chrome_options.add_argument("nwapp=" + testdir)
+
+datadir = os.path.join(testdir, "extract")
+if os.path.isdir(datadir) :
+  shutil.rmtree(datadir)
+
+driver = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER'], chrome_options=chrome_options)
+try:
+    print driver.current_url
+    time.sleep(2)
+    result = driver.find_element_by_id('result')
+    print result.get_attribute('innerHTML')
+    assert("success" in result.get_attribute('innerHTML'))
+finally:
+    driver.quit()
