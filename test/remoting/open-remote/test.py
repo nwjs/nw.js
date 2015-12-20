2df3d1a0e169a392785b63ed510fd937eac7c715
Roger Wang <wenrui@gmail.com>
2015-12-20 17:14:14 +0800

    [test] add open-remote

---
+++ test/remoting/open-remote/test.py
@@ -0,0 +1,46 @@
+import time
+import os
+import subprocess
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common import utils
+
+chrome_options = Options()
+chrome_options.add_argument("nwapp=" + os.path.dirname(os.path.abspath(__file__)))
+
+testdir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(testdir)
+
+port = str(utils.free_port())
+server = subprocess.Popen(['python', 'http-server.py', port])
+
+html = open('index.html', 'w')
+html.write('''
+<script>
+nw.Window.open('http://localhost:%s/remote.html', function(win) {
+  document.write('<h1 id="res">returned window is ' + typeof win + '</h1>');
+  win.y = 0;
+});
+</script>
+''' % (port))
+    
+html.close()
+
+driver = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER'], chrome_options=chrome_options)
+time.sleep(1)
+try:
+    print driver.current_url
+    time.sleep(1)
+    result = driver.find_element_by_id('res').get_attribute('innerHTML')
+    print result
+    assert("object" in result)
+    driver.switch_to_window(driver.window_handles[-1])
+    for id in ['res', 'res2', 'res3']:
+        result = driver.find_element_by_id(id).get_attribute('innerHTML')
+        print result
+        assert("DISABLED" in result)
+finally:
+    server.terminate()
+    driver.quit()
+
