22ff743ce6fe4ac0fd6820a50ad8c3b5d99fc194
Roger Wang <roger.wang@intel.com>
2015-12-28 12:43:54 +0800

    bump version to 0.13.0-beta1

--- src/nw_version.h
+++ src/nw_version.h
@@ -38,7 +38,7 @@
 #else
 # define NW_VERSION_STRING  NW_STRINGIFY(NW_MAJOR_VERSION) "." \
                               NW_STRINGIFY(NW_MINOR_VERSION) "." \
-                              NW_STRINGIFY(NW_PATCH_VERSION) "-alpha8"
+                              NW_STRINGIFY(NW_PATCH_VERSION) "-beta1"
 #endif
 
 #define NW_VERSION "v" NW_VERSION_STRING
