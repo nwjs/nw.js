3e1b44b5c6ca1e32558b9a5993b7ee6e00ca558e
Cong Liu <cong.liu@intel.com>
2016-01-04 15:22:21 +0800

    Copy methods of Node `require` to shim
    Fixed #4141

---
+++ test/remoting/require-methods/test.py
@@ -0,0 +1,23 @@
+import time
+import os
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+chrome_options.add_argument("nwapp=" + os.path.dirname(os.path.abspath(__file__)))
+
+driver = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER'], chrome_options=chrome_options)
+time.sleep(1)
+try:
+    print driver.current_url
+    result = driver.find_element_by_id('require-resolve').get_attribute('innerHTML')
+    print result
+    assert('success' in result)
+    result = driver.find_element_by_id('require-cache').get_attribute('innerHTML')
+    print result
+    assert('success' in result)
+    result = driver.find_element_by_id('require-extensions').get_attribute('innerHTML')
+    print result
+    assert('success' in result)
+finally:
+    driver.quit()
