56eda641b7f48f652c968a2aab1ee889e416cbbb
Roger Wang <roger.wang@intel.com>
2016-03-25 11:15:05 +0800

    bump version to 0.14.0-rc1

--- src/nw_version.h
+++ src/nw_version.h
@@ -22,8 +22,9 @@
 #define NW_VERSION_H
 
 #define NW_MAJOR_VERSION 0
-#define NW_MINOR_VERSION 13
-#define NW_PATCH_VERSION 5
+#define NW_MINOR_VERSION 14
+#define NW_PATCH_VERSION 0
+
 #define NW_VERSION_IS_RELEASE 0
 
 #ifndef NW_STRINGIFY
@@ -38,7 +39,7 @@
 #else
 # define NW_VERSION_STRING  NW_STRINGIFY(NW_MAJOR_VERSION) "." \
                               NW_STRINGIFY(NW_MINOR_VERSION) "." \
-                              NW_STRINGIFY(NW_PATCH_VERSION) "-pre"
+                              NW_STRINGIFY(NW_PATCH_VERSION) "-rc1"
 #endif
 
 #define NW_VERSION "v" NW_VERSION_STRING
