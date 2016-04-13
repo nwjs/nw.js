f231a60840a591bfd9140b136bf777bfdd0752fe
Cong Liu <cong.liu@intel.com>
2016-04-13 11:14:27 +0800

    Reordered path finding for backward compatible with nw12
    The path finding logic of NW.js app:
    1. Extract self
    2. <exe-folder>
    3. <exe-folder>/package.nw
    4. CLI arguments:
        * --nwapp
        * first argument
    5. default
    
    Fixed #4681

--- src/nw_package.cc
+++ src/nw_package.cc
@@ -171,25 +171,36 @@ std::wstring ASCIIToWide(const std::string& ascii) {
 Package::Package()
     : path_(),
       self_extract_(true) {
-  CommandLine* command_line = CommandLine::ForCurrentProcess();
-  // LOG(INFO) << command_line->GetCommandLineString();
-  // Try to load from the folder where the exe resides.
+
+  base::FilePath path;
+  
+  // 1. try to extract self
+  self_extract_ = true;
+  path = GetSelfPath();
+  if (InitFromPath(path))
+    return;
+
+  // 2. try to load from the folder where the exe resides.
   // Note: self_extract_ is true here, otherwise a 'Invalid Package' error
   // would be triggered.
-  base::FilePath path = GetSelfPath().DirName();
+  path = GetSelfPath().DirName();
 #if defined(OS_MACOSX)
   path = path.DirName().DirName().DirName();
 #endif
   if (InitFromPath(path))
     return;
 
+  // 3. try to load from <exe-folder>/package.nw
   path = path.AppendASCII("package.nw");
   if (InitFromPath(path))
     return;
 
-  // Then see if we have arguments and extract it.
+  // 4. see if we have arguments and extract it.
+  CommandLine* command_line = CommandLine::ForCurrentProcess();
+  // LOG(INFO) << command_line->GetCommandLineString();
   const base::CommandLine::StringVector& args = command_line->GetArgs();
 
+  // 4.1. try --nwapp= argument
   if (command_line->HasSwitch("nwapp")) {
     path = command_line->GetSwitchValuePath("nwapp");
     self_extract_ = false;
@@ -197,6 +208,7 @@ Package::Package()
       return;
   }
 
+  // 4.2 try first CLI argument
   if (args.size() > 0) {
     self_extract_ = false;
     path = FilePath(args[0]);
@@ -204,13 +216,7 @@ Package::Package()
       return;
   }
 
-  self_extract_ = true;
-  // Try to extract self.
-  path = GetSelfPath();
-  if (InitFromPath(path))
-    return;
-
-  // Finally we init with default settings.
+  // 5. init with default settings
   self_extract_ = false;
   InitWithDefault();
 }
