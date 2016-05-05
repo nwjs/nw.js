2ab672ed133d5e3c344211cf9c2e565e7a9b6b65
Roger Wang <roger.wang@intel.com>
2016-05-05 17:16:48 +0800

    [test] add issue4788-openremote-cdtopen for #4788

---
+++ test/sanity/issue4788-openremote-cdtopen/test.py
@@ -0,0 +1,64 @@
+import time
+import os
+import subprocess
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from nw_util import *
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common import utils
+from selenium.common.exceptions import NoSuchElementException
+
+chrome_options = Options()
+chrome_options.add_argument("nwapp=" + os.path.dirname(os.path.abspath(__file__)))
+
+testdir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(testdir)
+
+port = str(utils.free_port())
+server = subprocess.Popen(['python', 'http-server.py', port])
+
+html = open('index.html', 'w')
+html.write('''
+<html>
+<body>
+<script>
+chrome.developerPrivate.openDevTools({renderViewId: -1, renderProcessId: -1, extensionId: chrome.runtime.id});
+setTimeout(function() { 
+  nw.Window.open('http://localhost:%s/remote.html', function(win) {
+    document.write('<h1 id="res">returned window is ' + typeof win + '</h1>');
+  });
+}, 2000);
+</script>
+</body>
+</html>
+''' % (port))
+    
+html.close()
+
+run_counter = 6
+
+try:
+    while run_counter > 0 :
+        run_counter = run_counter - 1
+        driver = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER'], chrome_options=chrome_options)
+        time.sleep(1)
+        try:
+            print driver.current_url
+            wait_counter = 10
+            while wait_counter > 0 :
+                try:
+                    wait_counter = wait_counter - 1
+                    time.sleep(1)
+                    result = driver.find_element_by_id('res').get_attribute('innerHTML')
+                    break
+                except NoSuchElementException:
+                    pass
+            print result
+            assert("object" in result)
+        finally:
+            driver.quit()
+
+finally:
+    server.terminate()
