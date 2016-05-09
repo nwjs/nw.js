6130b8e2db138151ed57313cf66b99810a1323c2
Cong Liu <cong.liu@intel.com>
2016-05-09 14:29:28 +0800

    Fixup signing script

--- tools/mas/macstore_sign.sh
+++ tools/mas/macstore_sign.sh
@@ -5,6 +5,7 @@ if [ "$#" -lt "5" ]; then
   exit
 fi
 
+BASE_DIR="$(dirname $0)"
 SOURCE=$1
 DESTINATION=$2
 IDENTITY=$3
@@ -12,8 +13,8 @@ BUNDLEID=$4
 VERSION=$5
 BUNDLE_VERSION=$6
 
-ENTITLEMENTS_PARENT="$(sed "s/\$BUNDLEID/$BUNDLEID/" entitlements-parent.plist)"
-ENTITLEMENTS_CHILD="$(cat entitlements-child.plist)"
+ENTITLEMENTS_PARENT="$(sed "s/\$BUNDLEID/$BUNDLEID/" "$BASE_DIR/entitlements-parent.plist")"
+ENTITLEMENTS_CHILD="$(cat "$BASE_DIR/entitlements-child.plist")"
 
 # make a copy
 rm -rf "$DESTINATION/nwjs.app"
@@ -30,7 +31,6 @@ fi
 if [ -n "$BUNDLE_VERSION" ]; then
   OLD_VERSION="$(ls "$DESTINATION/nwjs.app/Contents/Versions")"
   OLD_BUNDLE_VERSION="${OLD_VERSION:5}"
-  echo "##### $OLD_BUNDLE_VERSION $BUNDLE_VERSION"
   find "$DESTINATION/nwjs.app" -name "Info.plist" -exec sed -i "" "s/$OLD_BUNDLE_VERSION/$BUNDLE_VERSION/" {} \;
 fi
 
@@ -38,5 +38,7 @@ fi
 find "$DESTINATION/nwjs.app" -name "Info.plist" -exec sed -i "" "s/io.nwjs.nw/$BUNDLEID/" {} \;
 
 # sign all executables
+echo "$ENTITLEMENTS_PARENT" > /tmp/entitlements.parent
+echo "$ENTITLEMENTS_CHILD" > /tmp/entitlements.child
 find "$DESTINATION/nwjs.app/Contents/Versions" -type f -perm +0111 -exec codesign -f --verbose -s "$IDENTITY" --entitlements /tmp/entitlements.child {} \;
 find "$DESTINATION/nwjs.app" \! -path "$DESTINATION/nwjs.app/Contents/Versions/*" -type f -perm +0111 -exec codesign -f --verbose -s "$IDENTITY" --entitlements /tmp/entitlements.parent {} \;
