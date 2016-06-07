8323e141dceffc7339f32efab3ff0f4eb1721386
Cong Liu <cong.liu@intel.com>
2016-06-07 16:19:39 +0800

    added new APIs for read/write multiple types of data to clipboard
    following new APIs added:
    * `clip.set(clipboardDataList)`
    * `clip.set(clipboardData)`
    * `clip.get(clipboardDataList)`
    * `clip.get(clipboardData)`
    
    fixed #4929

--- src/api/nw_clipboard_api.cc
+++ src/api/nw_clipboard_api.cc
@@ -27,146 +27,262 @@ namespace {
   const char* kPNGDataUriPrefix = "data:image/png;base64,";
   const char* kJPEGDataUriPrefix = "data:image/jpeg;base64,";
   const int   kQulity = 100;
-}
 
-NwClipboardGetSyncFunction::NwClipboardGetSyncFunction() {
-}
+  class ClipboardReader {
+  public:
+    ClipboardReader() {
+      clipboard_ = ui::Clipboard::GetForCurrentThread();
+    }
 
-NwClipboardGetSyncFunction::~NwClipboardGetSyncFunction() {
-}
+    bool Read(ClipboardData& data) {
+      switch(data.type) {
+        case TYPE_TEXT:
+        return ReadText(data);
+        break;
+        case TYPE_HTML:
+        return ReadHTML(data);
+        break;
+        case TYPE_RTF:
+        return ReadRTF(data);
+        break;
+        case TYPE_PNG:
+        case TYPE_JPEG:
+        return ReadImage(data);
+        break;
+        case TYPE_NONE:
+        NOTREACHED();
+        return false;
+      }
+      NOTREACHED();
+      return false;      
+    }
 
-bool NwClipboardGetSyncFunction::RunNWSync(base::ListValue* response, std::string* error) {
-  scoped_ptr<GetSync::Params> params(GetSync::Params::Create(*args_));
-  ui::Clipboard* clipboard = ui::Clipboard::GetForCurrentThread();
+    std::string error() {
+      return error_;
+    }
 
-  if (params->type == TYPE_TEXT) {
-    base::string16 text;
-    clipboard->ReadText(ui::CLIPBOARD_TYPE_COPY_PASTE, &text);
-    response->Append(new base::StringValue(text));
-    LOG(INFO) << "NwClipboardGetSyncFunction::RunSync(text)";
-    return true;
-  } else if (params->type == TYPE_PNG || params->type == TYPE_JPEG) {
-    std::vector<unsigned char> encoded_image;
-    SkBitmap bitmap = clipboard->ReadImage(ui::CLIPBOARD_TYPE_COPY_PASTE);
-    SkAutoLockPixels lock(bitmap);
-
-    if (bitmap.isNull()) {
-      response->Append(new base::StringValue(""));
+  private:
+    bool ReadText(ClipboardData& data) {
+      DCHECK(data.type == TYPE_TEXT);
+      base::string16 text;
+      clipboard_->ReadText(ui::CLIPBOARD_TYPE_COPY_PASTE, &text);
+      data.data.reset(new std::string(base::UTF16ToUTF8(text)));
       return true;
     }
 
-    if (params->type == TYPE_PNG &&
-       !gfx::PNGCodec::EncodeA8SkBitmap(bitmap, &encoded_image)) {
-      LOG(INFO) << "NwClipboardGetSyncFunction::RunSync(" << nwapi::nw__clipboard::ToString(params->type) << ") failed when converting to PNG";
-      *error = "Failed to encode as PNG";
-      return false;
-    } else if (params->type == TYPE_JPEG &&
-               !gfx::JPEGCodec::Encode(reinterpret_cast<const unsigned char*>(bitmap.getPixels()),
-                                      gfx::JPEGCodec::FORMAT_SkBitmap,
-                                      bitmap.width(),
-                                      bitmap.height(),
-                                      bitmap.rowBytes(),
-                                      kQulity,
-                                      &encoded_image)) {
-      LOG(INFO) << "NwClipboardGetSyncFunction::RunSync(" << nwapi::nw__clipboard::ToString(params->type) << ") failed when converting to JPEG";
-      *error = "Failed to encode as JPEG";
-      return false;
+    bool ReadHTML(ClipboardData& data) {
+      DCHECK(data.type == TYPE_HTML);
+      base::string16 text;
+      std::string src_url;
+      uint32_t fragment_start, fragment_end;
+      clipboard_->ReadHTML(ui::CLIPBOARD_TYPE_COPY_PASTE, &text, &src_url, &fragment_start, &fragment_end);
+      data.data.reset(new std::string(base::UTF16ToUTF8(text)));
+      return true;
     }
 
-    std::string encoded_image_base64;
-    std::string encoded_image_str(encoded_image.data(), encoded_image.data() + encoded_image.size());
-    base::Base64Encode(encoded_image_str, &encoded_image_base64);
+    bool ReadRTF(ClipboardData& data) {
+      DCHECK(data.type == TYPE_RTF);
+      std::string text;
+      clipboard_->ReadRTF(ui::CLIPBOARD_TYPE_COPY_PASTE, &text);
+      data.data.reset(new std::string(text));
+      return true;
+    }
 
-    if (!params->raw) {
-      if (params->type == TYPE_PNG) {
-        encoded_image_base64.insert(0, kPNGDataUriPrefix);
-      } else if (params->type == TYPE_JPEG) {
-        encoded_image_base64.insert(0, kJPEGDataUriPrefix);
+    bool ReadImage(ClipboardData& data) {
+      DCHECK(data.type == TYPE_PNG || data.type == TYPE_JPEG);
+      std::vector<unsigned char> encoded_image;
+      SkBitmap bitmap = clipboard_->ReadImage(ui::CLIPBOARD_TYPE_COPY_PASTE);
+      SkAutoLockPixels lock(bitmap);
+
+      if (bitmap.isNull()) {
+        return true;
       }
+
+      if (data.type == TYPE_PNG &&
+         !gfx::PNGCodec::EncodeA8SkBitmap(bitmap, &encoded_image)) {
+        LOG(INFO) << "NwClipboardGetSyncFunction::RunSync(" << nwapi::nw__clipboard::ToString(data.type) << ") failed when converting to PNG";
+        error_ = "Failed to encode as PNG";
+        return false;
+      } else if (data.type == TYPE_JPEG &&
+                 !gfx::JPEGCodec::Encode(reinterpret_cast<const unsigned char*>(bitmap.getPixels()),
+                                        gfx::JPEGCodec::FORMAT_SkBitmap,
+                                        bitmap.width(),
+                                        bitmap.height(),
+                                        bitmap.rowBytes(),
+                                        kQulity,
+                                        &encoded_image)) {
+        LOG(INFO) << "NwClipboardGetSyncFunction::RunSync(" << nwapi::nw__clipboard::ToString(data.type) << ") failed when converting to JPEG";
+        error_ = "Failed to encode as JPEG";
+        return false;
+      }
+
+      std::string encoded_image_base64;
+      std::string encoded_image_str(encoded_image.data(), encoded_image.data() + encoded_image.size());
+      base::Base64Encode(encoded_image_str, &encoded_image_base64);
+
+      if (!(data.raw.get() && *(data.raw))) {
+        if (data.type == TYPE_PNG) {
+          encoded_image_base64.insert(0, kPNGDataUriPrefix);
+        } else {
+          DCHECK(data.type == TYPE_JPEG);
+          encoded_image_base64.insert(0, kJPEGDataUriPrefix);
+        }
+      }
+
+      data.data.reset(new std::string(encoded_image_base64));
+      LOG(INFO) << "NwClipboardGetSyncFunction::RunSync(" << nwapi::nw__clipboard::ToString(data.type) << ")";
+      return true;
     }
 
-    response->Append(new base::StringValue(encoded_image_base64));
-    LOG(INFO) << "NwClipboardGetSyncFunction::RunSync(" << nwapi::nw__clipboard::ToString(params->type) << ")";
-    return true;
-  } else if (params->type == TYPE_HTML) {
-    base::string16 text;
-    std::string src_url;
-    uint32_t fragment_start, fragment_end;
-    clipboard->ReadHTML(ui::CLIPBOARD_TYPE_COPY_PASTE, &text, &src_url, &fragment_start, &fragment_end);
-    response->Append(new base::StringValue(text));
-    LOG(INFO) << "NwClipboardGetSyncFunction::RunSync(html)";
-    return true;
-  } else if (params->type == TYPE_RTF) {
-    std::string text;
-    clipboard->ReadRTF(ui::CLIPBOARD_TYPE_COPY_PASTE, &text);
-    response->Append(new base::StringValue(text));
-    LOG(INFO) << "NwClipboardGetSyncFunction::RunSync(rtf)";
-    return true;
-  } else {
-    NOTREACHED();
-    return false;
-  }
- 
-}
+    ui::Clipboard* clipboard_;
+    std::string error_;
+  };
 
-NwClipboardSetSyncFunction::NwClipboardSetSyncFunction() {
+  class ClipboardWriter {
+  public:
+    ClipboardWriter() {
+      scw_.reset(new ui::ScopedClipboardWriter(ui::CLIPBOARD_TYPE_COPY_PASTE));
+    }
 
-}
+    ~ClipboardWriter() {
+      scw_.reset();
+    }
 
-NwClipboardSetSyncFunction::~NwClipboardSetSyncFunction() {
-}
+    bool Write(ClipboardData& data) {
+      switch(data.type) {
+        case TYPE_TEXT:
+        return WriteText(data);
+        break;
+        case TYPE_HTML:
+        return WriteHTML(data);
+        break;
+        case TYPE_RTF:
+        return WriteRTF(data);
+        break;
+        case TYPE_PNG:
+        case TYPE_JPEG:
+        return WriteImage(data);
+        break;
+        case TYPE_NONE:
+        NOTREACHED();
+        return false;
+      }
+      NOTREACHED();
+      return false;
+    }
 
+    std::string error() const {
+      return error_;
+    }
 
-bool NwClipboardSetSyncFunction::RunNWSync(base::ListValue* response, std::string* error) {
-  scoped_ptr<SetSync::Params> params(SetSync::Params::Create(*args_));
-  ui::ScopedClipboardWriter scw(ui::CLIPBOARD_TYPE_COPY_PASTE);
+    void Reset() {
+      scw_->Reset();
+    }
 
-  if (params->type == TYPE_TEXT) {
-    scw.WriteText(base::UTF8ToUTF16(params->content));
-  } else if (params->type == TYPE_PNG || params->type == TYPE_JPEG) {
+  private:
+    bool WriteText(ClipboardData& data) {
+      DCHECK(data.type == TYPE_TEXT);
+      scw_->WriteText(base::UTF8ToUTF16(*(data.data)));
+      return true;
+    }
 
-    std::string content = params->content;
+    bool WriteHTML(ClipboardData& data) {
+      DCHECK(data.type == TYPE_HTML);
+      scw_->WriteHTML(base::UTF8ToUTF16(*(data.data)), std::string());
+      return true;
+    }
 
-    // strip off data uri header if raw is set
-    if (!params->raw) {
-      if (params->type == TYPE_PNG && base::StartsWith(content, kPNGDataUriPrefix, base::CompareCase::INSENSITIVE_ASCII)) {
+    bool WriteRTF(ClipboardData& data) {
+      DCHECK(data.type == TYPE_RTF);
+      scw_->WriteRTF(*(data.data));
+      return true;
+    }
+
+    bool WriteImage(ClipboardData& data) {
+      DCHECK(data.type == TYPE_PNG || data.type == TYPE_JPEG);
+      std::string content = *(data.data);
+
+      // strip off data uri header if raw is set
+      if (!(data.raw.get() && *(data.raw))) {
+        if (data.type == TYPE_PNG && base::StartsWith(content, kPNGDataUriPrefix, base::CompareCase::INSENSITIVE_ASCII)) {
           content = content.substr(strlen(kPNGDataUriPrefix));
-      } else if (params->type == TYPE_JPEG && base::StartsWith(content, kJPEGDataUriPrefix, base::CompareCase::INSENSITIVE_ASCII)) {
+        } else if (data.type == TYPE_JPEG && base::StartsWith(content, kJPEGDataUriPrefix, base::CompareCase::INSENSITIVE_ASCII)) {
           content = content.substr(strlen(kJPEGDataUriPrefix));
-      } else {
-        *error = base::StringPrintf("Invalid data URI. Only \"%s\" or \"%s\" is accepted.", kPNGDataUriPrefix, kJPEGDataUriPrefix);
+        } else {
+          error_ = base::StringPrintf("Invalid data URI. Only \"%s\" or \"%s\" is accepted.", kPNGDataUriPrefix, kJPEGDataUriPrefix);
+          return false;
+        }
+      }
+
+      std::string decoded_str;
+      if (!base::Base64Decode(content, &decoded_str)) {
+        error_ = "Bad base64 string";
         return false;
       }
-    }
 
-    std::string decoded_str;
-    if (!base::Base64Decode(content, &decoded_str)) {
-      *error = "Bad base64 string";
-      return false;
-    }
-
-    scoped_ptr<SkBitmap> bitmap(new SkBitmap());
-    if (params->type == TYPE_PNG &&
-      !gfx::PNGCodec::Decode(reinterpret_cast<const unsigned char*>(decoded_str.c_str()), decoded_str.size(), bitmap.get())) {
-      *error = "Failed to decode as PNG";
-      return false;
-    } else if (params->type == TYPE_JPEG) {
-      SkBitmap* tmp_bitmap = gfx::JPEGCodec::Decode(reinterpret_cast<const unsigned char*>(decoded_str.c_str()), decoded_str.size());
-      if (tmp_bitmap == NULL) {
-        *error = "Failed to decode as JPEG";
+      scoped_ptr<SkBitmap> bitmap(new SkBitmap());
+      if (data.type == TYPE_PNG &&
+        !gfx::PNGCodec::Decode(reinterpret_cast<const unsigned char*>(decoded_str.c_str()), decoded_str.size(), bitmap.get())) {
+        error_ = "Failed to decode as PNG";
         return false;
+      } else if (data.type == TYPE_JPEG) {
+        SkBitmap* tmp_bitmap = gfx::JPEGCodec::Decode(reinterpret_cast<const unsigned char*>(decoded_str.c_str()), decoded_str.size());
+        if (tmp_bitmap == NULL) {
+          error_ = "Failed to decode as JPEG";
+          return false;
+        }
+        bitmap.reset(tmp_bitmap);
       }
-      bitmap.reset(tmp_bitmap);
+
+      scw_->WriteImage(*bitmap);
+
+      return true;
     }
 
-    scw.WriteImage(*bitmap);
-  } else if (params->type == TYPE_HTML) {
-    scw.WriteHTML(base::UTF8ToUTF16(params->content), std::string());
-  } else if (params->type == TYPE_RTF) {
-    scw.WriteRTF(params->content);
-  } else {
-    NOTREACHED();
-    return false;
+    std::string error_;
+    scoped_ptr<ui::ScopedClipboardWriter> scw_;
+  };
+}
+
+NwClipboardGetListSyncFunction::NwClipboardGetListSyncFunction() {
+}
+
+NwClipboardGetListSyncFunction::~NwClipboardGetListSyncFunction() {
+}
+
+bool NwClipboardGetListSyncFunction::RunNWSync(base::ListValue* response, std::string* error) {
+  scoped_ptr<GetListSync::Params> params(GetListSync::Params::Create(*args_));
+  EXTENSION_FUNCTION_VALIDATE(params.get());
+  scoped_ptr<ClipboardReader> reader(new ClipboardReader());
+
+  for(auto& data : params->data) {
+    if (!reader->Read(*data)) {
+      *error = reader->error();
+      return false;
+    }
+    response->Append(data->ToValue());
+  }
+
+  return true;
+}
+
+NwClipboardSetListSyncFunction::NwClipboardSetListSyncFunction() {
+}
+
+NwClipboardSetListSyncFunction::~NwClipboardSetListSyncFunction() {
+}
+
+bool NwClipboardSetListSyncFunction::RunNWSync(base::ListValue* response, std::string* error) {
+  scoped_ptr<SetListSync::Params> params(SetListSync::Params::Create(*args_));
+  EXTENSION_FUNCTION_VALIDATE(params.get());
+  scoped_ptr<ClipboardWriter> writer(new ClipboardWriter());
+
+  for(auto& data : params->data) {
+    if (!writer->Write(*data)) {
+      *error = writer->error();
+      writer->Reset();
+      return false;
+    }
   }
 
   return true;
