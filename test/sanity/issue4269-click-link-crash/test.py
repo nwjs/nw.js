4299e0811d9bc8dcdf972f1a32dad8814370b6eb
Cong Liu <cong.liu@intel.com>
2016-09-07 15:03:32 +0800

    updated test cases for nw18 update
    Following test cases are affected by devtools update during nw18
    update:
    * issue3780-jailed-elements
    * issue3780-jailed
    * issue3835-inspect-crash
    * issue4121-inpect-node-crash
    * issue4269-click-link-crash
    
    fixed #5273 #5274 #5275 #5276

--- test/sanity/issue4269-click-link-crash/test.py
+++ test/sanity/issue4269-click-link-crash/test.py
@@ -36,7 +36,7 @@ try:
     print driver.window_handles
     print 'switch to devtools window'
     switch_to_devtools(driver, devtools_window=driver.window_handles[1]) # devtools comes to the 2nd
-    driver.execute_script('return document.querySelector(".inspector-view-tabbed-pane").shadowRoot.getElementById("tab-console")').click()
+    driver.execute_script('return document.querySelector(".tabbed-pane").shadowRoot.getElementById("tab-console")').click()
     driver.find_element_by_css_selector('.console-message-text .webkit-html-external-link').click()
     wait_window_handles(driver, 4)
     driver.switch_to_window(driver.window_handles[3])
