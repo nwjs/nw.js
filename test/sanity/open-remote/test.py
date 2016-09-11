13639181c2c63a800fdad754259b7282b0fbf63a
Roger Wang <roger.wang@intel.com>
2016-09-11 17:41:35 +0800

    [test] fix race condition in open-remote

--- test/sanity/open-remote/test.py
+++ test/sanity/open-remote/test.py
@@ -33,12 +33,13 @@ html.close()
 driver = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER'], chrome_options=chrome_options)
 time.sleep(1)
 try:
+    wait_window_handles(driver, 2)
+    driver.switch_to_window(driver.window_handles[0])
     print driver.current_url
     time.sleep(1)
     result = driver.find_element_by_id('res').get_attribute('innerHTML')
     print result
     assert("object" in result)
-    wait_window_handles(driver, 2)
     driver.switch_to_window(driver.window_handles[-1])
     for id in ['res', 'res2', 'res3']:
         result = driver.find_element_by_id(id).get_attribute('innerHTML')
