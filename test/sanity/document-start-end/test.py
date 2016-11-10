565f337b0919e2b76fb5a474c2d654ac0140c4d1
Roger Wang <roger.wang@intel.com>
2016-11-10 11:01:27 +0800

    [test] fix document-start-end with wait_for_element_id

--- test/sanity/document-start-end/test.py
+++ test/sanity/document-start-end/test.py
@@ -1,8 +1,12 @@
 import time
 import os
+import sys
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from nw_util import *
+
 chrome_options = Options()
 chrome_options.add_argument("nwapp=" + os.path.dirname(os.path.abspath(__file__)))
 
@@ -10,8 +14,8 @@ driver = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER'], chrome_opt
 time.sleep(1)
 try:
     print driver.current_url
-    result = driver.find_element_by_id('result').get_attribute('innerHTML')
-    result2 = driver.find_element_by_id('result2').get_attribute('innerHTML')
+    result = wait_for_element_id(driver, 'result')
+    result2 = wait_for_element_id(driver, 'result2')
     print result
     print result2
     assert(result == 'success from popup' and result2 == 'startiframe')
