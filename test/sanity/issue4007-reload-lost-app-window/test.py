b7589ce03d602e135acc46d631a992b47e208673
Roger Wang <roger.wang@intel.com>
2016-12-08 10:54:13 +0800

    [test] fix issue4007-reload-lost-app-window for nw20

--- test/sanity/issue4007-reload-lost-app-window/test.py
+++ test/sanity/issue4007-reload-lost-app-window/test.py
@@ -9,7 +9,9 @@ from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
 chrome_options.add_argument("nwapp=" + os.path.dirname(os.path.abspath(__file__)))
 
-driver = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER'], chrome_options=chrome_options)
+capabilities = {"pageLoadStrategy": "none"}
+
+driver = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER'], chrome_options=chrome_options, service_log_path="log", service_args=["--verbose"], desired_capabilities = capabilities)
 time.sleep(1)
 try:
     print driver.current_url
@@ -22,15 +24,14 @@ try:
     wait_window_handles(driver, lambda handles: len(handles) != 0 and handles[0] != old_handle)
     # devtools will be opened as the first window handle
     print driver.window_handles
-    print 'switch to devtools'
     switch_to_devtools(driver, devtools_window=driver.window_handles[0])
-    print 'close devtools'
-    driver.close()
-    wait_window_handles(driver, 1)
-    print driver.window_handles
-    print 'switch to main window'
-    driver.switch_to_window(driver.window_handles[0])
-    result = driver.find_element_by_id('result').get_attribute('innerHTML')
+    print 'click Console panel'
+    devtools_click_tab(driver, 'console')
+    print 'send_keys "document.getElementById(\'result\').innerHTML<enter>"'
+    time.sleep(1)
+    devtools_type_in_console(driver, 'document.getElementById(\'result\').innerHTML\n')
+    time.sleep(1)
+    result = driver.find_element_by_css_selector('.console-user-command-result .console-message-text .object-value-string').get_attribute('textContent')
     print result
     assert("success" in result)    
 finally:
