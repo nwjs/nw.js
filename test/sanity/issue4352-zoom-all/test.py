64a6e9a669cdd48267f518c89f1e3ad0bd379a4b
Roger Wang <roger.wang@intel.com>
2016-12-15 12:50:21 +0800

    [test] fix race condition in zoom-all test

--- test/sanity/issue4352-zoom-all/test.py
+++ test/sanity/issue4352-zoom-all/test.py
@@ -1,5 +1,8 @@
 import time
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from nw_util import *
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -10,14 +13,15 @@ driver = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER'], chrome_opt
 driver.implicitly_wait(5)
 time.sleep(1)
 try:
+    wait_window_handles(driver, 2)
+    wait_switch_window_name(driver, 'popup')
     print driver.current_url
-    driver.switch_to_window(driver.window_handles[-1])
     zoom2 = 'zoom level'
     while zoom2 == 'zoom level':
         zoom2 = driver.find_element_by_id('result').get_attribute('innerHTML')
         time.sleep(1)
     print "zoom2: ", zoom2
-    driver.switch_to_window(driver.window_handles[0])
+    wait_switch_window_name(driver, 'index')
     driver.find_element_by_id('get-zoom').click()
     zoom1 = driver.find_element_by_id('result').get_attribute('innerHTML')
     print "zoom1: ", zoom1
