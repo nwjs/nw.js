7b454961895152db0a51754e454504cd9464af7e
Roger Wang <roger.wang@intel.com>
2017-02-16 14:43:59 +0800

    [test] guard nwjc with checking source code and strings

--- test/sanity/nwjc/test.py
+++ test/sanity/nwjc/test.py
@@ -2,6 +2,8 @@ import time
 import os
 import shutil
 import subprocess
+import platform
+from subprocess import Popen, PIPE
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -19,6 +21,12 @@ assert(False == os.path.isfile(binfile))
 subprocess.call([nwjc, "mytest.js", "mytest.bin"])
 assert(os.path.isfile(binfile))
 
+if platform.system() == 'Linux':
+    proc = Popen(['strings', 'mytest.bin'], stdout=PIPE, stderr=PIPE)
+    out, err = proc.communicate()
+    print out
+    assert("42" not in out)
+
 driver = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER'], chrome_options=chrome_options)
 try:
     print driver.current_url
@@ -26,5 +34,8 @@ try:
     result = driver.find_element_by_id('result')
     print result.get_attribute('innerHTML')
     assert("44" == result.get_attribute('innerHTML'))
+    result2 = driver.find_element_by_id('result2')
+    print result2.get_attribute('innerHTML')
+    assert("native code" in result2)
 finally:
     driver.quit()
