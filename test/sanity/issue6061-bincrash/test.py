4c16dca633b304178a7f8049e474b630cf793afc
Roger Wang <roger.wang@intel.com>
2017-10-30 13:14:59 +0800

    [test] add case for #6061: nwjc bin crash

---
+++ test/sanity/issue6061-bincrash/test.py
@@ -0,0 +1,47 @@
+import time
+import os
+import shutil
+import subprocess
+import platform
+import sys
+from subprocess import Popen, PIPE
+from selenium.webdriver.common.by import By
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from nw_util import *
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+testdir = os.path.dirname(os.path.abspath(__file__))
+chrome_options.add_argument("nwapp=" + testdir)
+binfile = os.path.join(testdir, "index.bin")
+nwjc = os.path.join(os.path.dirname(os.environ['CHROMEDRIVER']), "nwjc.exe" if os.name == "nt" else "nwjc")
+os.chdir(testdir)
+try:
+  os.remove(binfile)
+except:
+  pass
+assert(False == os.path.isfile(binfile))
+subprocess.call([nwjc, "index.js", "index.bin"])
+assert(os.path.isfile(binfile))
+
+driver = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER'], chrome_options=chrome_options)
+driver.implicitly_wait(2)
+time.sleep(1)
+try:
+    switch_to_app(driver)
+    print driver.current_url
+    print 'wait for devtools open'
+    wait_window_handles(driver, 2)
+    print driver.window_handles
+    print 'switch to devtools'
+    switch_to_devtools(driver)
+    print 'click Console panel'
+    devtools_click_tab(driver, 'console')
+    driver.find_element_by_class_name('console-message-text').click()
+    driver.find_element_by_class_name('console-view-object-properties-section').click()
+    outer = driver.execute_script('return document.querySelector(".console-view-object-properties-section").shadowRoot')
+    outer.find_element_by_class_name("object-properties-section-dimmed").click()
+finally:
+    driver.quit()
