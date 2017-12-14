a00bd01262cbddd7a57189dc32e4ed89c1467f1e
Roger Wang <roger.wang@intel.com>
2017-12-14 13:05:38 +0800

    [test] fix flaky issue4199-cookie

--- test/sanity/issue4199-cookie/test.py
+++ test/sanity/issue4199-cookie/test.py
@@ -12,11 +12,9 @@ chrome_options.add_argument("nwapp=" + os.path.dirname(os.path.abspath(__file__)
 
 driver = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER'], chrome_options=chrome_options)
 driver.implicitly_wait(2)
-time.sleep(1)
 try:
     print driver.current_url
-    cookie = driver.find_element_by_id('cookie').get_attribute('innerHTML')
+    cookie = wait_for_element_id_content(driver, 'cookie', 'Hi there')
     print cookie
-    assert ('Hi there' in cookie)
 finally:
     driver.quit()
