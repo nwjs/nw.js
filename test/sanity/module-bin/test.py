da790fd616b6bd0490027564f05eb433ef2159d1
Roger Wang <roger@nwjs.io>
2018-01-08 10:50:22 +0800

    [test] fix module-bin: wait for result properly

--- test/sanity/module-bin/test.py
+++ test/sanity/module-bin/test.py
@@ -4,6 +4,9 @@ import shutil
 import subprocess
 import platform
 from subprocess import Popen, PIPE
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from nw_util import *
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -31,10 +34,9 @@ if platform.system() == 'Linux':
 driver = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER'], chrome_options=chrome_options)
 try:
     print driver.current_url
-    result = driver.find_element_by_id('result')
-    print result.get_attribute('innerHTML')
-    assert("52" in result.get_attribute('innerHTML'))
-    result2 = driver.find_element_by_id('source').get_attribute('innerHTML')
+    result = wait_for_element_id_content(driver, 'result', '52')
+    print result
+    result2 = wait_for_element_id_content(driver, 'source', 'native code')
     print result2
     assert("{ [native code] }" in result2)
 finally:
