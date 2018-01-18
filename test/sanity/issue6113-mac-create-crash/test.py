8babf7808b4f995220d7c86243c2e2f20e9e434c
wanghongjuan <hongjuan.wang@intel.com>
2018-01-18 14:22:56 +0800

    [test] Add test for issue#6113
    - Add test for issue #6113
    - This test is failed on failed version 0.24.3, pass on fixed version 0.27.4

---
+++ test/sanity/issue6113-mac-create-crash/test.py
@@ -0,0 +1,34 @@
+import time
+import os
+import platform
+import sys
+
+if platform.system() != 'Darwin':
+    print 'Skipped for non Mac platform'
+    sys.exit(0)
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from nw_util import *
+
+chrome_options = Options()
+chrome_options.add_argument("nwapp=" + os.path.dirname(os.path.abspath(__file__)))
+
+driver = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER'], chrome_options=chrome_options)
+driver.implicitly_wait(2)
+try:
+    wait_window_handles(driver, 2)
+    handles = driver.window_handles
+    print handles
+    driver.switch_to.window(handles[1])
+    output = driver.current_url
+    print output
+    print 'waiting for crash'
+    time.sleep(5)
+    assert driver.title == "Google"
+    assert("https://www.google.com/" in output)
+    print 'There is no crash'
+finally:
+    driver.quit()
