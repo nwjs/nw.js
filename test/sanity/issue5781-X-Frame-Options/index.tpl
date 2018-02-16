f0457d04d969545198c3598b80cd1f941f76b224
Kevin Fan <kf@gnor.net>
2018-02-16 17:10:37 +0800

    [test] fix race condition in issue5781-X-Frame-Options

--- test/sanity/issue5781-X-Frame-Options/index.tpl
+++ test/sanity/issue5781-X-Frame-Options/index.tpl
@@ -1,6 +1,6 @@
 <html>
  <body>
-  <iframe src="http://localhost:{port}" nwfaketop ></iframe>
+  <iframe src="http://localhost:{port}" nwfaketop id="iframe_a"></iframe>
   <script>
       document.write('<h1 id="res1">Node is ' + (typeof nw === 'undefined' ? 'DISABLED': 'ENABLED') + '</h1>');
     </script>
--- test/sanity/issue5781-X-Frame-Options/test.py
+++ test/sanity/issue5781-X-Frame-Options/test.py
@@ -26,16 +26,29 @@ html = open('index.html', 'w')
 html.write(content)
 html.close()
 
-driver = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER'], chrome_options=chrome_options)
+driver = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER'], chrome_options=chrome_options, service_log_path="log", service_args=["--verbose"])
 driver.implicitly_wait(5)
 try:
     print driver.current_url
     print 'loading http server in iframe element'
-    wait_for_element_tag(driver, "iframe")
-    assert(driver.find_element_by_tag_name("iframe") is not None)
-    driver.switch_to_frame(driver.find_element_by_tag_name("iframe"))
-    res2 = wait_for_element_id(driver, 'res2')
-    assert( "Node is DISABLED" in res2)
+    wait_for_element_id(driver, "iframe_a")
+    driver.switch_to_frame("iframe_a")
+    ret = ''
+    timeout = 10
+    while timeout > 0:
+        try:
+            ret = driver.find_element_by_id('res2').get_attribute('innerHTML')
+            break
+        except selenium.common.exceptions.NoSuchElementException:
+            driver.switch_to_default_content()
+            driver.switch_to_frame("iframe_a")
+        except selenium.common.exceptions.WebDriverException:
+            pass
+        time.sleep(1)
+        timeout = timeout - 1
+        if timeout <= 0:
+             raise Exception('Timeout when waiting for element res2')
+    assert( "Node is DISABLED" in ret)
     print 'page iframe is loaded'
 finally:
     server.terminate()
