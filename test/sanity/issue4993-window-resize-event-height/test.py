a08215c2af3959c2b612aaf7c80f0a489d67509c
wanghongjuan <hongjuan.wang@intel.com>
2018-05-16 15:33:16 +0800

    [test] Add test for issue4993
    - Add test for issue #4993
    - This test is failed on v0.15.4, passed on v0.30.4

---
+++ test/sanity/issue4993-window-resize-event-height/test.py
@@ -0,0 +1,23 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from nw_util import *
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+
+chrome_options = Options()
+chrome_options.add_argument("nwapp=" + os.path.dirname(os.path.abspath(__file__)))
+
+driver = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER'], chrome_options=chrome_options)
+driver.implicitly_wait(2)
+try:
+    print driver.current_url
+    element = driver.find_element_by_id('resize-window')
+    ActionChains(driver).click(element).perform()
+    res = wait_for_element_id(driver, "resize")
+    print res
+    assert("width: 777, height: 555" in res)
+finally:
+    driver.quit()
