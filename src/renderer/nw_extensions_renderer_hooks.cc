3ac76daa54e1fe8bc7f68fb4d921769c5a7925ff
Roger Wang <roger@nwjs.io>
2019-01-17 23:09:17 -0700

    [nw36] Fix new-win-policy event: ignore

--- src/renderer/nw_extensions_renderer_hooks.cc
+++ src/renderer/nw_extensions_renderer_hooks.cc
@@ -580,8 +580,9 @@ void willHandleNavigationPolicy(content::RenderView* rv,
   if (!val->IsString())
     return;
   v8::String::Utf8Value policy_str(isolate, val);
-  //FIXME:: handle "ignore"
-  if (!strcmp(*policy_str, "download"))
+  if (!strcmp(*policy_str, "ignore"))
+    *policy = blink::kWebNavigationPolicyIgnore;
+  else if (!strcmp(*policy_str, "download"))
     *policy = blink::kWebNavigationPolicyDownload;
   else if (!strcmp(*policy_str, "current"))
     *policy = blink::kWebNavigationPolicyCurrentTab;
