8fa0d16f8b0a7a50cdf24b20e423572145baa6ca
Roger Wang <roger@nwjs.io>
2019-10-05 00:38:05 -0600

    fixup! rebase to Chromium 78

--- src/api/menu/menu_mac.mm
+++ src/api/menu/menu_mac.mm
@@ -21,6 +21,7 @@
 #include "content/nw/src/api/menu/menu.h"
 
 #include "base/message_loop/message_loop.h"
+#include "base/message_loop/message_loop_current.h"
 #include "base/mac/scoped_sending_event.h"
 #include "base/values.h"
 #import <Cocoa/Cocoa.h>
