8cdc459e1dada5bacdb0b1ea0b26875221c63c52
Roger Wang <roger@nwjs.io>
2019-11-13 01:52:26 -0700

    [test] add issue7173-iframe
    Ref #7173

---
+++ test/sanity/issue7173-iframe/test.py
@@ -0,0 +1,20 @@
+import time
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from nw_util import *
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("nwapp=" + os.path.dirname(os.path.abspath(__file__)))
+
+driver = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER'], chrome_options=chrome_options)
+driver.implicitly_wait(2)
+try:
+    print driver.current_url
+    cookie = wait_for_element_id_content(driver, 'ret', 'nwjs.io', 20)
+    print cookie
+finally:
+    driver.quit()
