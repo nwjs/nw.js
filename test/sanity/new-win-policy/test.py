18fd9a0d406df12d3dc2f789b3e8d6d2a21d275e
Roger Wang <roger@nwjs.io>
2019-11-17 21:36:08 +0800

    [test] update new-win-policy: support ctrl-click; frameless

--- test/sanity/new-win-policy/test.py
+++ test/sanity/new-win-policy/test.py
@@ -6,6 +6,9 @@ from nw_util import *
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+
 chrome_options = Options()
 chrome_options.add_argument("nwapp=" + os.path.dirname(os.path.abspath(__file__)))
 
@@ -21,6 +24,22 @@ try:
     driver.switch_to_window(driver.window_handles[-1])
     output = driver.find_element_by_id('output')
     assert(output.get_attribute('value') == '["inject-js-start","body-start","inject-js-end","onload-dom"]')
+    output2 = driver.find_element_by_id('output2').get_attribute('innerHTML')
+    assert("Success" in output2)
+    
+    driver.close()
+    driver.switch_to_window(driver.window_handles[0])
+    link = driver.find_element_by_link_text('new window')
+    if platform.system() == 'Darwin':
+        ActionChains(driver).key_down(Keys.SHIFT).click(link).key_up(Keys.SHIFT).perform()
+    else:
+        ActionChains(driver).key_down(Keys.CONTROL).click(link).key_up(Keys.CONTROL).perform()
+    wait_window_handles(driver, 2)
+    driver.switch_to_window(driver.window_handles[-1])
+    output = driver.find_element_by_id('output')
+    assert(output.get_attribute('value') == '["inject-js-start","body-start","inject-js-end","onload-dom"]')
+    output2 = driver.find_element_by_id('output2').get_attribute('innerHTML')
+    assert("Success" in output2)
+   
 finally:
-    #time.sleep(50)
     driver.quit()
