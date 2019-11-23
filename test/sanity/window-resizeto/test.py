58d329860f6e67b8f232a62f32f5fe0018407fb4
Roger Wang <roger@nwjs.io>
2019-11-23 00:09:09 -0700

    [test] fix window-resizeto: handle multiple windows

--- test/sanity/window-resizeto/test.py
+++ test/sanity/window-resizeto/test.py
@@ -17,14 +17,25 @@ try:
     print driver.current_url
     driver.implicitly_wait(10)
     wait_window_handles(driver, 2)
+    popup_window = None
+    main_window = None
+    for handle in driver.window_handles:
+      driver.switch_to_window(handle)
+      print driver.title
+      if 'new popup' in driver.title:
+        popup_window = handle
+      else:
+        main_window = handle
+    assert(popup_window)
+    assert(main_window)
     print 'switch to opened window'
-    driver.switch_to_window(driver.window_handles[-1])
+    driver.switch_to_window(popup_window)
     result = wait_for_element_id(driver, 'yellow')
     print 'window size: %s' % result
     assert('200, 300' in result)
-    driver.switch_to_window(driver.window_handles[0])
+    driver.switch_to_window(main_window)
     driver.find_element_by_id('btn_resizeto').click()
-    driver.switch_to_window(driver.window_handles[-1])
+    driver.switch_to_window(popup_window)
     result = driver.find_element_by_id('yellow').get_attribute('innerHTML')
     print 'window size: %s' % result
     assert('180, 180' in result)
