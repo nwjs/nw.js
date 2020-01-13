f2d5c65b6bb819407e824a8fc4ad53509b118dad
Roger Wang <roger@nwjs.io>
2020-01-13 11:04:25 +0800

    [test] fix window-id: test without chromedriver

--- test/sanity/window-id/test.py
+++ test/sanity/window-id/test.py
@@ -16,42 +16,33 @@ try:
 except:
     pass
 
-chrome_options = Options()
-chrome_options.add_argument("nwapp=" + os.path.dirname(os.path.abspath(__file__)))
-chrome_options.add_argument("user-data-dir=" + datadir)
-
-driver = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER'], chrome_options=chrome_options)
-driver.implicitly_wait(5)
-last_pos = ""
 try:
-    res = wait_for_element_id(driver, "yellow")
-    print res
-    assert(res == "800, 600")
-    driver.find_element_by_id('resize').click()
-    res = wait_for_element_id(driver, "yellow")
-    print res
-    assert(res == "345, 345")
-    driver.find_element_by_id('move').click()
-    driver.find_element_by_id('getxy').click()
-    res = wait_for_element_id(driver, "xy")
-    print res
-    last_pos = res
-
-finally:
-    driver.quit()
-
-driver = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER'], chrome_options=chrome_options)
-driver.implicitly_wait(5)
+    os.remove('last_pos.txt')
+except:
+    pass
 try:
-    res = wait_for_element_id(driver, "yellow")
-    print res
-    assert(res == "345, 345")
-    driver.find_element_by_id('getxy').click()
-    res = wait_for_element_id(driver, "xy")
-    print res
-    #FIXME: linux is good without chromedriver
-    if not 'linux' in sys.platform:
-      assert (res == last_pos)
-finally:
-    driver.quit()
+    os.remove('succeed.txt')
+except:
+    pass
+try:
+    os.remove('failed.txt')
+except:
+    pass
 
+pkg1 = os.path.dirname(os.environ['CHROMEDRIVER'])
+
+if platform.system() == 'Darwin':
+    exe = os.path.join(pkg1, 'nwjs.app', 'Contents', 'MacOS', 'nwjs')
+elif platform.system() == 'Linux':
+    exe = os.path.join(pkg1, 'nw')
+else:
+    exe = os.path.join(pkg1, 'nw.exe')
+
+
+p = subprocess.call([exe, "--user-data-dir="+datadir, "."])
+
+assert(os.path.exists('last_pos.txt'))
+
+p = subprocess.call([exe, "--user-data-dir="+datadir, "."])
+
+assert(os.path.exists('succeed.txt'))
