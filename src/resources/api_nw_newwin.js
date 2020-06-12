dfc200a2916e0c1a33b40c57b1e0841abf5aa42e
Roger Wang <roger@nwjs.io>
2020-06-12 02:37:06 -0600

    Fix #7488: NW2: window blur event does not execute multiple event listeners

--- src/resources/api_nw_newwin.js
+++ src/resources/api_nw_newwin.js
@@ -176,12 +176,12 @@ NWWindow.prototype.on = function (event, callback, record) {
     this.cWindow = currentNWWindowInternal.getCurrent(this.cWindow.id, {'populate': true});
     var cbf = wrap(function(windowId) {
       if (self.cWindow.id === windowId) {
-        self.cWindow.focused = true;
+        callback.__nw_cb.focused = true;
         return;
       }
-      if (!self.cWindow.focused)
+      if (!callback.__nw_cb.focused)
         return;
-      self.cWindow.focused = false;
+      callback.__nw_cb.focused = false;
       callback.call(self);
     });
     chrome.windows.onFocusChanged.addListener(cbf);
