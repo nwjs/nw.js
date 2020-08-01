c01fdcd986e121d6daa3dbe089e0d2db76a3b9d8
Roger Wang <roger@nwjs.io>
2020-07-31 21:26:23 -0600

    include v8_binding_for_core.h in nw_custom_bindings.cc

--- src/nw_custom_bindings.cc
+++ src/nw_custom_bindings.cc
@@ -88,6 +88,7 @@ using namespace blink;
 #include "third_party/blink/renderer/core/script/modulator.h"
 #include "third_party/blink/renderer/core/script/js_module_script.h"
 #include "third_party/blink/renderer/core/script/module_record_resolver.h"
+#include "third_party/blink/renderer/bindings/core/v8/v8_binding_for_core.h"
 
 //#include "third_party/WebKit/Source/core/inspector/InspectorInstrumentation.h"
 //#include "third_party/WebKit/Source/core/inspector/InspectorResourceAgent.h"
