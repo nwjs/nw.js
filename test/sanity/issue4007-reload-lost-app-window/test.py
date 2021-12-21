ec6eee339ebaed94157d2ccc306bea4fc44d61e9
Roger Wang <roger@nwjs.io>
2021-12-21 15:31:32 +0000

    [test] fix issue4007-reload-lost-app-window devtools typing

--- test/sanity/issue4007-reload-lost-app-window/test.py
+++ test/sanity/issue4007-reload-lost-app-window/test.py
@@ -23,21 +23,9 @@ try:
     wait_window_handles(driver, lambda handles: len(handles) != 0 and handles[0] != old_handle)
     # devtools will be opened as the first window handle
     print driver.window_handles
-    try:
-        switch_to_devtools(driver, devtools_window=driver.window_handles[0])
-    except:
-        if len(driver.window_handles) < 2:
-            driver.find_element_by_id('opendevtools').click()
-            wait_window_handles(driver, lambda handles: len(handles) == 2)
-        switch_to_devtools(driver, devtools_window=driver.window_handles[1])
-    print 'click Console panel'
-    devtools_click_tab(driver, 'console')
-    print 'send_keys "document.getElementById(\'result\').innerHTML<enter>"'
-    time.sleep(1)
-    devtools_type_in_console(driver, 'document.getElementById(\'result\').innerHTML\n')
-    time.sleep(1)
-    result = driver.find_element_by_css_selector('.console-user-command-result .console-message-text .object-value-string').get_attribute('textContent')
+    driver.switch_to_window(driver.window_handles[0])
+    result = driver.find_element_by_id('result').get_attribute('innerHTML')
     print result
-    assert("success" in result)    
+    assert("success" in result)
 finally:
     driver.quit()
