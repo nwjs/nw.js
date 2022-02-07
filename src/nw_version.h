bf0a20d878e59a0fadac7699b53897483fa500d3
Roger Wang <roger@nwjs.io>
2022-02-07 03:41:56 +0000

    bump version: 0.61.1

--- src/nw_version.h
+++ src/nw_version.h
@@ -1,16 +1,16 @@
 // Copyright (c) 2012 Roger Wang <roger@nwjs.io>
 //                    Zhao Cheng <zcbenz@gmail.com>
-// 
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy 
 // of this software and associated documentation files (the "Software"), to deal
 //  in the Software without restriction, including without limitation the rights
 //  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell co
 // pies of the Software, and to permit persons to whom the Software is furnished
 //  to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in al
 // l copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IM
 // PLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNES
 // S FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
@@ -23,7 +23,7 @@
 
 #define NW_MAJOR_VERSION 0
 #define NW_MINOR_VERSION 61
-#define NW_PATCH_VERSION 0
+#define NW_PATCH_VERSION 1
 
 #define NW_VERSION_IS_RELEASE 1
 
