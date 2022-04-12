acc15ab72bc5e456983addb3f857877e0bcdeb3c
Roger Wang <roger@nwjs.io>
2022-04-12 17:47:00 -0600

    migrate to gfx::Insets::TLBR

--- src/browser/browser_view_layout.cc
+++ src/browser/browser_view_layout.cc
@@ -46,10 +46,10 @@ gfx::Size BrowserViewLayout::GetPreferredSize(const View* host) const {
   gfx::Rect rect(web_view_->GetPreferredSize());
   rect.Inset(-host->GetInsets());
   if (menu_bar_)
-    rect.Inset(0, 0, 0, -kMenuHeight);
+    rect.Inset(gfx::Insets::TLBR(0, 0, 0, -kMenuHeight));
 
   if (tool_bar_)
-    rect.Inset(0, 0, 0, -tool_bar_->GetPreferredSize().height());
+    rect.Inset(gfx::Insets::TLBR(0, 0, 0, -tool_bar_->GetPreferredSize().height()));
 
   return rect.size();
 }
