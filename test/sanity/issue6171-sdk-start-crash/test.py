a0425bf0ea5be24b4cba06713baf4593c603c496
Roger Wang <roger@nwjs.io>
2023-01-18 15:34:38 -0700

    [test] fix issue6171-sdk-start-crash

--- test/sanity/issue6171-sdk-start-crash/test.py
+++ test/sanity/issue6171-sdk-start-crash/test.py
@@ -8,12 +8,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 chrome_options = Options()
 chrome_options.add_argument("nwapp=" + os.path.dirname(os.path.abspath(__file__)))
+capabilities = {"pageLoadStrategy": "none"}
 
-driver = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER'], chrome_options=chrome_options)
+driver = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER'], chrome_options=chrome_options, desired_capabilities = capabilities, service_log_path="log", service_args=["--verbose"])
 driver.implicitly_wait(2)
 try:
     print('waiting for crash')
-
+    wait_switch_window_url(driver, 'index.html')
     result = wait_for_element_id(driver, 'versions')
     #print result
     assert('NWjs version' in result)
