1016af6db87304942df8c489ce78f54394fcf6c7
Ayushman Chhabra <ayushmxn@outlook.com>
2023-01-19 15:57:51 -0500

    remove redundant files

--- test/sanity/dom-filelist-append/test.py
+++ test/sanity/dom-filelist-append/test.py
@@ -13,7 +13,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 testdir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(testdir)
 
-port = "3000"
+port = str(utils.free_port())
 server = subprocess.Popen(['python3', 'http-server.py', port])
 
 chrome_options = Options()
--- test/sanity/dom-input-files-append/node-main.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.use(express.static("./"));
-app.listen(3000);
