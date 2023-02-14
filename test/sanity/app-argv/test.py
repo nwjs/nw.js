c9367349490004a092c21f31cebab23dc723ebea
Ayushman Chhabra <ayushmxn@outlook.com>
2023-02-13 21:22:47 -0500

    update app-argv deps

--- test/sanity/app-argv/test.py
+++ test/sanity/app-argv/test.py
@@ -1,20 +1,20 @@
-import time
 import os
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
 chrome_options = Options()
 chrome_options.add_argument("nwapp=" + os.path.dirname(os.path.abspath(__file__)))
 
-driver = webdriver.Chrome(executable_path=os.environ['CHROMEDRIVER'], chrome_options=chrome_options, service_log_path="log", service_args=["--verbose"])
+driver = webdriver.Chrome(service=Service(executable_path=os.environ['CHROMEDRIVER']), options=chrome_options, service_log_path="log", service_args=["--verbose"])
 driver.implicitly_wait(5)
 try:
     print(driver.current_url)
-    result = driver.find_element_by_id('result-argv').get_attribute('innerHTML')
-    print('nw.App.argv = %s' % result)
+    result = driver.find_element('result-argv').get_attribute('innerHTML')
     assert('nwapp=' not in result)
-    result = driver.find_element_by_id('result-fullargv').get_attribute('innerHTML')
-    print('nw.App.fullArgv = %s' % result)
+    result = driver.find_element(By.ID, 'result-fullargv').get_attribute('innerHTML')
     assert('nwapp=' in result)
 finally:
     driver.quit()
