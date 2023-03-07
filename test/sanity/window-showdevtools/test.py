d76cdc93620db136f2d576a661609435939cb827
Ayushman Chhabra <ayushmxn@outlook.com>
2023-03-07 03:55:44 -0500

    test: only run test if flavor is not sdk

--- test/sanity/window-showdevtools/test.py
+++ test/sanity/window-showdevtools/test.py
@@ -9,12 +9,16 @@ chrome_options = Options()
 chrome_options.add_argument("nwapp=" + os.path.dirname(os.path.abspath(__file__)))
 
 driver = webdriver.Chrome(
-    service=Service(os.environ['CHROMEDRIVER']),
+    service=Service(
+        executable_path=os.environ["CHROMEDRIVER"]
+    ),
     options=chrome_options
 )
 driver.implicitly_wait(5)
 try:
     flavor = driver.find_element(By.ID, 'flavor').get_attribute('innerHTML')
-    assert(flavor == 'normal')
+    if (flavor == "normal"):
+        result = driver.find_element(By.ID, 'result').get_attribute('innerHTML')
+        assert(result == 'showDevTools does not exist on normal build flavor.')
 finally:
     driver.quit()
