cd89214e0ea77b8771465733dd3186834d5244ad
tharatau <ayushmxn@outlook.com>
2023-04-17 20:55:49 -0400

    [Resources/Window] Throw error if build flavor not `sdk`

--- src/resources/api_nw_window.js
+++ src/resources/api_nw_window.js
@@ -373,7 +373,7 @@ apiBridge.registerCustomHook(function(bindingsAPI) {
 
     NWWindow.prototype.showDevTools = function(frm, callback) {
       if (process.versions['nw-flavor'] !== 'sdk')
-      return new Error('showDevTools does not exist on normal build flavor.');
+      throw new Error('showDevTools does not exist on normal build flavor.');
       var id = '';
       if (typeof frm === 'string')
         id = frm;
