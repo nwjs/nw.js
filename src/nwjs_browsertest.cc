1420d2984b3ef059e4d52e0cf5030aa370b27ae0
Roger Wang <roger@nwjs.io>
2023-10-25 18:43:38 -0600

    remove chrome_notification_types.h from nwjs_browsertest.cc

--- src/nwjs_browsertest.cc
+++ src/nwjs_browsertest.cc
@@ -29,7 +29,6 @@
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/apps/platform_apps/app_browsertest_util.h"
 #include "chrome/browser/chrome_content_browser_client.h"
-#include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/content_settings/host_content_settings_map_factory.h"
 #include "chrome/browser/download/download_history.h"
 #include "chrome/browser/download/download_prefs.h"
