d25f78b25d827c18b2ef7f113527868b4716cce2
Roger Wang <roger@nwjs.io>
2024-02-10 15:49:29 -0700

    [test] update webview-cdt-ext with new devtools
    https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5224229

--- test/sanity/webview-cdt-ext/test.py
+++ test/sanity/webview-cdt-ext/test.py
@@ -43,8 +43,8 @@ try:
     title = ''
     while timeout > 0 and not found:
         try:
-            driver.execute_script('UI.inspectorView.tabbedPane.selectTab(UI.inspectorView.tabbedPane.tabs[10].id)')
-            title = driver.execute_script('return UI.inspectorView.tabbedPane.tabs[10].title')
+            driver.execute_script('UI.inspectorView.tabbedPane.selectTab(UI.inspectorView.tabbedPane.tabs[9].id)')
+            title = driver.execute_script('return UI.inspectorView.tabbedPane.tabs[9].title')
         except:
             pass
         print("title:", title)
