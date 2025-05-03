d6e4dac5c73d0b6c6130c2f4697664f0a2c1457f
Roger Wang <roger@nwjs.io>
2025-05-03 08:59:27 -0600

    [test] update issue4131-form-post

--- test/sanity/issue4131-form-post/server.js
+++ test/sanity/issue4131-form-post/server.js
@@ -1,20 +1,22 @@
 var http = require('http');
 
-function startServer(port, cb) {
+function startServer(cb) {
 
   var server = http.createServer();
-
+  let requests = 0;
   server.on('request', function(req, res) {
     console.log(req.url);
     if (req.url === '/post.html') {
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.write('<html><body>');
       res.write('<h1 id="method">method: ' + req.method + '</h1>');
+      res.write('<h1 id="reqno">reqno: ' + requests + '</h1>');
+      requests++;
       req.on('data', function(data) {
         res.write('<div id="data">' + data + '</div>');
       });
       req.on('end', function() {
-        res.write('<form method="POST" action="http://localhost:' + port + '/post.html">');
+        res.write('<form method="POST" action="http://localhost:' + server.address().port + '/post.html">');
         res.write('<input type="text" name="myfield" value="myvalue2">');
         res.write('<button id="submit" type="submit">Submit</button>');
         res.write('</form>');
@@ -26,5 +28,5 @@ function startServer(port, cb) {
     }
   });
 
-  server.listen(port, cb || function() {});
+  server.listen(0, cb || function() {});
 }
--- test/sanity/issue4131-form-post/test.py
+++ test/sanity/issue4131-form-post/test.py
@@ -1,5 +1,8 @@
 import time
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from nw_util import *
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -13,14 +16,18 @@ driver.implicitly_wait(5)
 try:
     driver.implicitly_wait(10)
     print((driver.current_url))
-    driver.execute_script('startServer(%s)' % utils.free_port())
-    result = driver.find_element_by_id('server-started').get_attribute('innerHTML')
+    driver.execute_script('startServer()')
+    result = wait_for_element_id(driver, 'server-started')
     print('server started')
     driver.find_element_by_id('submit').click()
+    result = wait_for_element_id(driver, 'reqno');
+    print(result)
     result = driver.find_element_by_id('method').get_attribute('innerHTML')
     print(result)
     assert('POST' in result)
     driver.find_element_by_id('submit').click()
+    result = wait_for_element_id_content(driver, 'reqno', 'reqno: 1');
+    print(result)
     result = driver.find_element_by_id('method').get_attribute('innerHTML')
     print(result)
     assert('POST' in result)
