b5b207ffdc220ccd7e0ccdcf2ede68433f859a7a
Kevin Fan <kf@gnor.net>
2026-04-19 13:20:46 -0600

    add test for URL with single quote crash (#8286)

---
+++ test/sanity/issue8286-single-quote-url/test.py
@@ -0,0 +1,20 @@
+import time
+import os
+import sys
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from nw_util import *
+from selenium.webdriver.common.by import By
+
+chrome_options = Options()
+chrome_options.add_argument('nwapp=' + os.path.dirname(os.path.abspath(__file__)))
+driver = get_configured_webdriver(chrome_options_instance=chrome_options)
+driver.implicitly_wait(5)
+try:
+    wait_window_handles(driver, 1)
+    driver.switch_to.window(driver.window_handles[0])
+    wait_for_element_id_content(driver, 'result', 'PASS', timeout=15)
+    print('PASS: navigated to URL with single quote without crash')
+finally:
+    driver.quit()
